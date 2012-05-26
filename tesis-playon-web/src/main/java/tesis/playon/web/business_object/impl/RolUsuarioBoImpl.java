1d58a07f5744a29cc4396be3e45c5a6637f9a497
morales.batovski <morales.batovski@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-05-26 20:28:53 +0000

    CRUD de un rol de usuario por Hibernate.

--- tesis-playon-web/src/main/java/tesis/playon/web/business_object/impl/RolUsuarioBoImpl.java
+++ tesis-playon-web/src/main/java/tesis/playon/web/business_object/impl/RolUsuarioBoImpl.java
@@ -1,11 +1,13 @@
 package tesis.playon.web.business_object.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tesis.playon.web.business_object.RolUsuarioBo;
 import tesis.playon.web.business_object.dao.RolUsuarioDao;
 import tesis.playon.web.model.RolUsuario;
 
+@Service("rolUsuarioBo")
 public class RolUsuarioBoImpl implements RolUsuarioBo {
 
     @Autowired
@@ -20,11 +22,11 @@ public class RolUsuarioBoImpl implements RolUsuarioBo {
     }
 
     public void update(RolUsuario rolUsuario) {
-	rolUsuarioDao.save(rolUsuario);
+	rolUsuarioDao.update(rolUsuario);
     }
 
     public void delete(RolUsuario rolUsuario) {
-	rolUsuarioDao.save(rolUsuario);
+	rolUsuarioDao.delete(rolUsuario);
     }
 
     public RolUsuario findByNombreRolUsuario(String nombreRolUsuario) {
