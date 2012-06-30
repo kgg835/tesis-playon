88cad30dec1bd2a42824829cb531c214011313f4
alebostico@gmail.com <alebostico@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-06-30 23:16:17 +0000

    Se crearó la clase de negocio EstadoPublicidad y el DAO correspondiente

---
+++ tesis-playon-web/src/main/java/tesis/playon/web/business_object/dao/impl/EstadoPublicidadDao.java
@@ -0,0 +1,34 @@
+package tesis.playon.web.business_object.dao.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import tesis.playon.util.CustomHibernateDaoSupport;
+import tesis.playon.web.business_object.dao.IEstadoPublicidadDao;
+import tesis.playon.web.model.EstadoPublicidad;
+
+@Repository("estadoPublicidadDao")
+public class EstadoPublicidadDao extends CustomHibernateDaoSupport implements IEstadoPublicidadDao {
+
+    @Override
+    public void save(EstadoPublicidad estadoPublicidad) {
+	getHibernateTemplate().save(estadoPublicidad);
+    }
+
+    @Override
+    public void update(EstadoPublicidad estadoPublicidad) {
+	getHibernateTemplate().update(estadoPublicidad);
+    }
+
+    @Override
+    public void delete(EstadoPublicidad estadoPublicidad) {
+	getHibernateTemplate().delete(estadoPublicidad);
+    }
+
+    @Override
+    public EstadoPublicidad findByNombreEstadoPublicidad(String nombreEstadoPublicidad) {
+	List<?> list = getHibernateTemplate().find("from EstadoPublicidad where nombre=?", nombreEstadoPublicidad);
+	return (EstadoPublicidad) list.get(0);
+    }
+}
