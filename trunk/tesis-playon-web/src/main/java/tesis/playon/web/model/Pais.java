f092e02912e4eac9b5202bd715673c25930a5a39
pablo.moreno.la31 <pablo.moreno.la31@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-03 22:24:22 +0000

    Pais y provincia, ejemplo de FK.

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/Pais.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/Pais.java
@@ -17,9 +17,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-
 /**
- * Clase de negocio que contiene los diferentes Países.
+ * Clase de negocio que contiene los diferentes Paises.
  * 
  * @author gmorales
  * @date 03/07/2012
@@ -31,68 +30,47 @@ public class Pais implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Integer id;
-
-    private String nombre;
-    
-    private HashSet<Provincia> provincias;
-
-    /**
-     * Constructor con parámetros.
-     * 
-     * @param id
-     * @param nombre
-     */
-    public Pais(Integer id, String nombre) {
-	this.id = id;
-	this.nombre = nombre;
-    }
-
-    /**
-     * Devuelve el ID del objeto.
-     * 
-     * @return El ID del objeto.
-     */
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "paisID", nullable = false)
+    private Integer id;
+
+    @Column(name = "nombre", unique = true, nullable = false, length = 50)
+    private String nombre;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "Provincia", joinColumns = { @JoinColumn(name = "paisID", unique = true) }, inverseJoinColumns = { @JoinColumn(name = "paisID") })
+    private HashSet<Provincia> provincias = new HashSet<Provincia>(0);
+
+    public Pais(Integer id, String nombre, HashSet<Provincia> provincias) {
+	this.id = id;
+	this.nombre = nombre;
+	this.provincias = provincias;
+    }
+
     public Integer getId() {
 	return id;
     }
 
-    /**
-     * Devuelve el nombre del objeto.
-     * 
-     * @return El nombre del objeto.
-     */
-    @Column(name = "nombre", unique = true, nullable = false, length = 50)
     public String getNombre() {
 	return nombre;
     }
 
-    /**
-     * Setea un nuevo nombre al objeto.
-     * 
-     * @param nombre
-     *            El nombre del objeto.
-     */
     public void setNombre(String nombre) {
 	this.nombre = nombre;
     }
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "price_tag_section", joinColumns = { @JoinColumn(name = "price_tag_id", unique = true) }, inverseJoinColumns = { @JoinColumn(name = "section_id") })
     public HashSet<Provincia> getProvincias() {
-        return provincias;
+	return provincias;
     }
 
     public void setProvincias(HashSet<Provincia> provincias) {
-        this.provincias = provincias;
+	this.provincias = provincias;
     }
-    
+
     @Override
     public String toString() {
-	return "País [paisID=" + id + ", nombre=" + nombre + "]";
+	return "Pais [paisID=" + id + ", nombre=" + nombre + ", Provincias=" + provincias.toString() + "]";
     }
 
 }
