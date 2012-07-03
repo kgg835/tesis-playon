b7d63b3a897da849222514657a68977e624e99ef
morales.batovski@gmail.com <morales.batovski@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-03 20:48:12 +0000

    Beans de país y provincia.

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/Pais.java
@@ -0,0 +1,79 @@
+package tesis.playon.web.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * Clase de negocio que contiene los diferentes Países.
+ * 
+ * @author gmorales
+ * @date 03/07/2012
+ * 
+ */
+@Entity
+@Table(name = "Pais", catalog = "tesis_playon", uniqueConstraints = { @UniqueConstraint(columnNames = "nombre") })
+public class Pais implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String nombre;
+
+    /**
+     * Constructor con parámetros.
+     * 
+     * @param id
+     * @param nombre
+     */
+    public Pais(Integer id, String nombre) {
+	this.id = id;
+	this.nombre = nombre;
+    }
+
+    /**
+     * Devuelve el ID del objeto.
+     * 
+     * @return El ID del objeto.
+     */
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "paisID", nullable = false)
+    public Integer getId() {
+	return id;
+    }
+
+    /**
+     * Devuelve el nombre del objeto.
+     * 
+     * @return El nombre del objeto.
+     */
+    @Column(name = "nombre", unique = true, nullable = false, length = 50)
+    public String getNombre() {
+	return nombre;
+    }
+
+    /**
+     * Setea un nuevo nombre al objeto.
+     * 
+     * @param nombre
+     *            El nombre del objeto.
+     */
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+	return "País [paisID=" + id + ", nombre=" + nombre + "]";
+    }
+
+}
