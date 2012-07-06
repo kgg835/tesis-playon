e4d4c4b33ad2a8b653cbb75d29d256fa2e5c7186
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-06 05:25:18 +0000

    DAO e implem TipoDoc

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/business_object/dao/impl/TipoDocDao.java
@@ -0,0 +1,35 @@
+package tesis.playon.web.business_object.dao.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import tesis.playon.util.CustomHibernateDaoSupport;
+import tesis.playon.web.business_object.dao.ITipoDocDao;
+import tesis.playon.web.model.TipoDoc;
+
+/**
+ * 
+ * @author garribere
+ *
+ */
+@Repository("tipo_doc")
+public class TipoDocDao extends CustomHibernateDaoSupport implements ITipoDocDao {
+
+    public void save(TipoDoc tipoDoc) {
+	getHibernateTemplate().save(tipoDoc);
+    }
+
+    public void update(TipoDoc tipoDoc) {
+	getHibernateTemplate().update(tipoDoc);
+    }
+
+    public void delete(TipoDoc tipoDoc) {
+	getHibernateTemplate().delete(tipoDoc);
+    }
+
+    public TipoDoc findByNombreTipoDoc(String nombreTipoDoc) {
+	List<?> list = getHibernateTemplate().find("from TipoDoc where nombre=?", nombreTipoDoc);
+	return (TipoDoc) list.get(0);
+    }
+}
