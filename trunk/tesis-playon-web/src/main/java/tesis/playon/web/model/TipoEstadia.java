8cde7108369226bce1e87fff2b0816aaf5b3d54c
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-06 02:39:21 +0000

    dao e implement de la clase TipoEstadia

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/TipoEstadia.java
@@ -0,0 +1,79 @@
+package tesis.playon.web.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * Clase de negocio que contiene los diferentes tipos de estadia.
+ * 
+ * @author garribere
+ * @date 05/07/2012
+ * 
+ */
+@Entity
+@Table(name = "tipo_estadia", catalog = "tesis_playon", uniqueConstraints = { @UniqueConstraint(columnNames = "nombre") })
+public class TipoEstadia implements Serializable {
+
+    private static final long serialVersionU	ID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "tipoEstadiaID")
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    
+    public TipoEstadia() {
+    }
+
+    public TipoEstadia(String nombre, String descripcion) {
+	this.nombre = nombre;
+	this.descripcion=descripcion;
+    }
+
+    public Integer getId() {
+	return id;
+    }
+
+    public void setId(Integer id) {
+	this.id = id;
+    }
+
+    public String getNombre() {
+	return nombre;
+    }
+
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
+
+    public void getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    @Override
+    public String toString() {
+	return "TipoEstadia [TipoEstadiaID=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
+    }
+
+}
