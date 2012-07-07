95fc28d74b19b1e04a5706742e3254456f82ef2c
alebostico@gmail.com <alebostico@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-07 19:43:55 +0000

    Minor Bug Fix. Se usa la clase Date con toString() en los métodos toString() de las clases Comentario, DenunciaPlaya y DenunciaVehiculo.

--- tesis-playon-web/src/main/java/tesis/playon/web/model/DenunciaVehiculo.java
+++ tesis-playon-web/src/main/java/tesis/playon/web/model/DenunciaVehiculo.java
@@ -21,30 +21,30 @@ import javax.persistence.Table;
  * Clase de negocio que contiene la denuncia hac�a un vehiculo
  * 
  * @author Pablo
- *
+ * 
  */
 
 @Entity
 @Table(name = "denuncia_vehiculo", catalog = "tesis_playon")
-public class DenunciaVehiculo implements Serializable{
-    
+public class DenunciaVehiculo implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "denunciaVehiculoID", nullable = false)
     private Integer id;
-    
+
     @Column(name = "asunto", unique = false, nullable = false)
     private String asunto;
-    
+
     @Column(name = "fechaAlta", unique = true, nullable = false)
     private Date fechaAlta;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vehiculoID", nullable = false)
     private Vehiculo vehiculo;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "playaID", nullable = false)
     private Playa playa;
@@ -57,45 +57,45 @@ public class DenunciaVehiculo implements Serializable{
     }
 
     public String getAsunto() {
-        return asunto;
+	return asunto;
     }
 
     public void setAsunto(String asunto) {
-        this.asunto = asunto;
+	this.asunto = asunto;
     }
 
     public Date getFechaAlta() {
-        return fechaAlta;
+	return fechaAlta;
     }
 
     public void setFechaAlta(Date fechaAlta) {
-        this.fechaAlta = fechaAlta;
+	this.fechaAlta = fechaAlta;
     }
 
     public Vehiculo getVehiculo() {
-        return vehiculo;
+	return vehiculo;
     }
 
     public void setVehiculo(Vehiculo vehiculo) {
-        this.vehiculo = vehiculo;
+	this.vehiculo = vehiculo;
     }
 
     public Playa getPlaya() {
-        return playa;
+	return playa;
     }
 
     public void setPlaya(Playa playa) {
-        this.playa = playa;
+	this.playa = playa;
     }
 
     public Integer getId() {
-        return id;
+	return id;
     }
 
     @Override
     public String toString() {
-	return "DenunciaVehiculo [denunciaVehiculoID=" + id + ", asunto=" + asunto + ", fechaAlta=" + fechaAlta + ", Vehiculo="+ vehiculo.getPatente() + ", Playa=" + playa + "]";
+	return "DenunciaVehiculo [denunciaVehiculoID=" + id + ", asunto=" + asunto + ", fechaAlta="
+		+ fechaAlta.toString() + ", Vehiculo=" + vehiculo.getPatente() + ", Playa=" + playa + "]";
     }
 
-   
 }
