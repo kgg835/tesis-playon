d1d72f5b36f8ad7cdf453e3b013d82695adfce75
pablo.moreno.la31 <pablo.moreno.la31@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-07 20:09:30 +0000

    Dao e impl Foto

---
+++ tesis-playon-web/src/main/java/tesis/playon/web/model/Foto.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package tesis.playon.web.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author Pablo
+ *
+ */
+@Entity
+@Table(name = "foto", catalog = "tesis_playon")
+public class Foto implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "fotoID")
+    private int id;
+    
+    @Column(name = "descripcion")
+    private String descripcion;
+    
+    @Column(name = "link")
+    private String link;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "perfilPlayaID")
+    private PerfilPlaya perfilPlaya;
+
+    public Foto(String descripcion, String link, PerfilPlaya perfilPlaya) {
+	this.descripcion = descripcion;
+	this.link = link;
+	this.perfilPlaya = perfilPlaya;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public PerfilPlaya getPerfilPlaya() {
+        return perfilPlaya;
+    }
+
+    public void setPerfilPlaya(PerfilPlaya perfilPlaya) {
+        this.perfilPlaya = perfilPlaya;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+	return "Foto [fotoID=" + id + ", descripcion=" + descripcion + ", link=" + link + ", perfilPlayaID=" + perfilPlaya.getId()
+		+ "]";
+    }
+}
