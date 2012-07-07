e00d1fe102c4fdbc09d91889373a78095a9cd141
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-07 20:56:26 +0000

    Actualizacion de los archivos Hibernate.xml y Hibernate.cfg.xml

--- tesis-playon-web/src/main/java/tesis/playon/common/App.java
+++ tesis-playon-web/src/main/java/tesis/playon/common/App.java
@@ -14,68 +14,71 @@ public class App {
     public static void main(String[] args) {
 	ApplicationContext appContext = new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
 
-	// Test para Rol Usuario
-	IRolUsuarioBo rolUsuarioBo = (IRolUsuarioBo) appContext.getBean("rolUsuarioBo");
-
-	/** insert **/
-	RolUsuario rolUsuario = new RolUsuario();
-	rolUsuario.setNombre("Cliente");
-	rolUsuario.setDescripcion("Usuario con el perfil de cliente.");
-	rolUsuarioBo.save(rolUsuario);
-
-	/** select **/
-	RolUsuario otroRolUsuario = rolUsuarioBo.findByNombreRolUsuario("Cliente");
-	System.out.println(otroRolUsuario);
-
-	/** update **/
-	otroRolUsuario.setNombre("Administrador");
-	rolUsuario.setDescripcion("Usuario con el perfil de administrador.");
-	rolUsuarioBo.update(otroRolUsuario);
-
-	/** delete **/
-	rolUsuarioBo.delete(otroRolUsuario);
-
-	// Test para Pais
-	IPaisDao paisDao = (IPaisDao) appContext.getBean("paisDao");
-
-	/** insert **/
-	Pais pais = new Pais("Argentina");
-	pais.setNombre("Argentina");
-	paisDao.save(pais);
-
-	/** select **/
-	Pais otroPais = paisDao.findByNombrePais("Argentina");
-	System.out.println(otroPais);
-
-	/** update **/
-	otroPais.setNombre("Republica Argentina");
-	paisDao.update(otroPais);
-
-	// Test para Provincia
-	IProvinciaDao provinciaDao = (IProvinciaDao) appContext.getBean("provinciaDao");
-
-	/** insert **/
-	Provincia provincia = new Provincia("Cordoba", otroPais);
-	provinciaDao.save(provincia);
-
-	/** select **/
-	Provincia otroProvincia = provinciaDao.findByNombreProvincia("Cordoba");
-	System.out.println(otroProvincia.toString());
-
-	/** update **/
-	otroProvincia.setNombre("Buenos Aires");
-	provinciaDao.update(otroProvincia);
-
-	/** select **/
-	otroPais = paisDao.findByNombrePais("Republica Argentina");
-	System.out.println(otroPais);
-
+//	// Test para Rol Usuario
+//	IRolUsuarioBo rolUsuarioBo = (IRolUsuarioBo) appContext.getBean("rolUsuarioBo");
+//
+//	/** insert **/
+//	RolUsuario rolUsuario = new RolUsuario();
+//	rolUsuario.setNombre("Cliente");
+//	rolUsuario.setDescripcion("Usuario con el perfil de cliente.");
+//	rolUsuarioBo.save(rolUsuario);
+//
+//	/** select **/
+//	RolUsuario otroRolUsuario = rolUsuarioBo.findByNombreRolUsuario("Cliente");
+//	System.out.println(otroRolUsuario);
+//
+//	/** update **/
+//	otroRolUsuario.setNombre("Administrador");
+//	rolUsuario.setDescripcion("Usuario con el perfil de administrador.");
+//	rolUsuarioBo.update(otroRolUsuario);
+//
+//	/** delete **/
+//	rolUsuarioBo.delete(otroRolUsuario);
+//
+//	// Test para Pais
+//	IPaisDao paisDao = (IPaisDao) appContext.getBean("paisDao");
+//
+//	/** insert **/
+//	Pais pais = new Pais("Argentina");
+//	pais.setNombre("Argentina");
+//	paisDao.save(pais);
+//
+//	/** select **/
+//	Pais otroPais = paisDao.findByNombrePais("Argentina");
+//	System.out.println(otroPais);
+//
+//	/** update **/
+//	otroPais.setNombre("Republica Argentina");
+//	paisDao.update(otroPais);
+//
+//	// Test para Provincia
+//	IProvinciaDao provinciaDao = (IProvinciaDao) appContext.getBean("provinciaDao");
+//
+//	/** insert **/
+//	Provincia provincia = new Provincia("Cordoba", otroPais);
+//	provinciaDao.save(provincia);
+//
+//	/** select **/
+//	Provincia otroProvincia = provinciaDao.findByNombreProvincia("Cordoba");
+//	System.out.println(otroProvincia.toString());
+//
+//	/** update **/
+//	otroProvincia.setNombre("Buenos Aires");
+//	provinciaDao.update(otroProvincia);
+//
+//	/** select **/
+//	otroPais = paisDao.findByNombrePais("Republica Argentina");
+//	System.out.println(otroPais);
+//
 //	/** delete **/
 //	provinciaDao.delete(otroProvincia);
 //
 //	/** delete **/
 //	paisDao.delete(otroPais);
+//	
+	
 
 	System.out.println("Done");
+	
     }
 }
