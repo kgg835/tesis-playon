5f9bf3acd605f471ec91ac63ef9758576d755543
alebostico@gmail.com <alebostico@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-08 22:06:01 +0000

    Clase Playa implementación y DAO.

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/Estadia.java
@@ -0,0 +1,7 @@
+package tesis.playon.web.model;
+
+import java.io.Serializable;
+
+public class Estadia implements Serializable {
+
+}
--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/Playa.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/model/Playa.java
@@ -3,10 +3,150 @@
  */
 package tesis.playon.web.model;
 
-/**
- * @author Pablo
- *
- */
-public class Playa {
+import static javax.persistence.GenerationType.IDENTITY;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * @author Alejandro
+ * @date 08/07/2012
+ */
+@Entity
+@Table(name = "playa", catalog = "tesis_playon")
+public class Playa implements Serializable {
+
+    private static final long serialVersionUID = -5969681339752247843L;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "playaID")
+    private Integer id;
+
+    @Column(name = "cuit")
+    private String cuit;
+
+    @Column(name = "disponibilidad")
+    private Integer disponibilidad;
+
+    @Column(name = "domicilio")
+    private String domicilio;
+
+    @Column(name = "nombreComercial")
+    private String nombreComercial;
+
+    @Column(name = "razonSocial")
+    private String razonSocial;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "barrioID")
+    private Barrio barrio;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estadoID")
+    private EstadoPlaya estado;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estadiaID")
+    private Estadia estadia;
+
+    public Playa() {
+	super();
+    }
+
+    public Playa(String cuit, Integer disponibilidad, String domicilio, String nombreComercial, String razonSocial,
+	    Barrio barrio, EstadoPlaya estado, Estadia estadia) {
+	super();
+	this.cuit = cuit;
+	this.disponibilidad = disponibilidad;
+	this.domicilio = domicilio;
+	this.nombreComercial = nombreComercial;
+	this.razonSocial = razonSocial;
+	this.barrio = barrio;
+	this.estado = estado;
+	this.estadia = estadia;
+    }
+
+    public String getCuit() {
+	return cuit;
+    }
+
+    public void setCuit(String cuit) {
+	this.cuit = cuit;
+    }
+
+    public Integer getDisponibilidad() {
+	return disponibilidad;
+    }
+
+    public void setDisponibilidad(Integer disponibilidad) {
+	this.disponibilidad = disponibilidad;
+    }
+
+    public String getDomicilio() {
+	return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+	this.domicilio = domicilio;
+    }
+
+    public String getNombreComercial() {
+	return nombreComercial;
+    }
+
+    public void setNombreComercial(String nombreComercial) {
+	this.nombreComercial = nombreComercial;
+    }
+
+    public String getRazonSocial() {
+	return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+	this.razonSocial = razonSocial;
+    }
+
+    public Barrio getBarrio() {
+	return barrio;
+    }
+
+    public void setBarrio(Barrio barrio) {
+	this.barrio = barrio;
+    }
+
+    public EstadoPlaya getEstado() {
+	return estado;
+    }
+
+    public void setEstado(EstadoPlaya estado) {
+	this.estado = estado;
+    }
+
+    public Estadia getEstadia() {
+	return estadia;
+    }
+
+    public void setEstadia(Estadia estadia) {
+	this.estadia = estadia;
+    }
+
+    public Integer getId() {
+	return id;
+    }
+
+    @Override
+    public String toString() {
+	return "Playa [playaID=" + id + ", cuit=" + cuit + ", nombreComercial=" + nombreComercial + ", razonSocial="
+		+ razonSocial + ", domicilio=" + domicilio + "]";
+    }
 }
