11d2c7df944fa3d7058d688a9026f31b6a42ee56
pablo.moreno.la31 <pablo.moreno.la31@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-09 07:09:44 +0000

    Test de Prueba de UsuarioSistema y sus modificaciones DAO e implement

--- tesis-playon-web/src/test/java/Testing/TestUsuarioSistema.java
+++ tesis-playon-web/src/test/java/Testing/TestUsuarioSistema.java
@@ -9,8 +9,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import tesis.playon.web.business_object.dao.IRolUsuarioDao;
+import tesis.playon.web.business_object.dao.IUsuarioDao;
 import tesis.playon.web.business_object.dao.IUsuarioSistemaDao;
 import tesis.playon.web.model.RolUsuario;
+import tesis.playon.web.model.Usuario;
 import tesis.playon.web.model.UsuarioSistema;
 
 /**
@@ -22,54 +24,47 @@ public class TestUsuarioSistema {
     public static void main(String[] args) {
 	ApplicationContext appContext = new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
 	
+	IUsuarioDao usuarioDao = (IUsuarioDao) appContext.getBean("usuarioDao");
 	IRolUsuarioDao rolUsuarioDao = (IRolUsuarioDao) appContext.getBean("rolUsuarioDao");
 	/** insert **/
-	RolUsuario rolUsuario = new RolUsuario("Socio", "Usuario con el perfil de cliente.");
-	rolUsuarioDao.save(rolUsuario);
-	rolUsuario = rolUsuarioDao.findByNombreRolUsuario("Socio");
-	System.out.println(rolUsuario);
-	RolUsuario rolUsuario1 = new RolUsuario("Anonimo", "Usuario no registrado en el sistema.");
-	rolUsuarioDao.save(rolUsuario1);
-	rolUsuario1 = rolUsuarioDao.findByNombreRolUsuario("Anonimo");
-	RolUsuario rolUsuario2 = new RolUsuario("Invitado", null);
-	rolUsuarioDao.save(rolUsuario2);
-	rolUsuario2 = rolUsuarioDao.findByNombreRolUsuario("Invitado");
+	RolUsuario rolUsuario = rolUsuarioDao.findByNombreRolUsuario("Administrador");
+	RolUsuario rolUsuario1 = rolUsuarioDao.findByNombreRolUsuario("Auditor");
+	RolUsuario rolUsuario2 = rolUsuarioDao.findByNombreRolUsuario("usuario");
+	Usuario usuario = usuarioDao.findByNombreUsuario("pablo_la_31");
+	Usuario usuario1 = usuarioDao.findByNombreUsuario("pablo_la31");
 	
 	
-//	IUsuarioSistemaDao usuarioSistemaDao = (IUsuarioSistemaDao) appContext.getBean("usuarioSistemaDao");
-//
-//	/** insert **/
-//	UsuarioSistema usuarioSistema = new UsuarioSistema(rolUsuario);
-//	UsuarioSistema usuarioSistema1 = new UsuarioSistema(rolUsuario1);
-//	usuarioSistemaDao.save(usuarioSistema);
-//	usuarioSistemaDao.save(usuarioSistema1);
-//	List<UsuarioSistema> usuarios = usuarioSistemaDao.findAll();
-//	System.out.println("\nUsuarios del Sistema:");
-//	for (UsuarioSistema _usuario : usuarios) {
-//	    System.out.println(_usuario);
-//	}
-//
-//	/** select **/
-//	UsuarioSistema otroUsuario = usuarioSistemaDao.findByNombreUsuarioSistema("Anonimo");
-//	System.out.println("\nEncontrado:\t" + otroUsuario);
-//	
-//	/** update **/
-//	usuarioSistema.setRolUsuario(rolUsuario2);
-//	usuarioSistemaDao.update(usuarioSistema);
-//	System.out.println("\nModificado:\t" + usuarioSistemaDao.findByNombreUsuarioSistema("Invitado"));
-//	
-//	System.out.println("\n");
-//	/** delete **/
-//	usuarioSistemaDao.delete(usuarioSistema);
-//	usuarioSistemaDao.delete(usuarioSistema1);
-//	usuarios = usuarioSistemaDao.findAll();
-//	System.out.println("\nRestantes:");
-//	for (UsuarioSistema _usuario : usuarios) {
-//	    System.out.println(_usuario);
-//	}
-	rolUsuarioDao.delete(rolUsuario);
-	rolUsuarioDao.delete(rolUsuario1);
-	rolUsuarioDao.delete(rolUsuario2);
+	IUsuarioSistemaDao usuarioSistemaDao = (IUsuarioSistemaDao) appContext.getBean("usuarioSistemaDao");
+
+	/** insert **/
+	UsuarioSistema usuarioSistema = new UsuarioSistema(rolUsuario, usuario);
+	UsuarioSistema usuarioSistema1 = new UsuarioSistema(rolUsuario1, usuario1);
+	usuarioSistemaDao.save(usuarioSistema);
+	usuarioSistemaDao.save(usuarioSistema1);
+	List<UsuarioSistema> usuarios = usuarioSistemaDao.findAll();
+	System.out.println("\nUsuarios del Sistema:");
+	for (UsuarioSistema _usuario : usuarios) {
+	    System.out.println(_usuario);
+	}
+
+	/** select **/
+	UsuarioSistema otroUsuario = usuarioSistemaDao.findByNombreUsuarioSistema(""+usuarioSistema.getUsuario());
+	System.out.println("\nEncontrado:\t" + otroUsuario);
+	
+	/** update **/
+	usuarioSistema.setRolUsuario(rolUsuario2);
+	usuarioSistemaDao.update(usuarioSistema);
+	System.out.println("\nModificado:\t" + usuarioSistemaDao.findByNombreUsuarioSistema(""+usuarioSistema.getUsuario()));
+	
+	System.out.println("\n");
+	/** delete **/
+	usuarioSistemaDao.delete(usuarioSistema);
+	usuarioSistemaDao.delete(usuarioSistema1);
+	usuarios = usuarioSistemaDao.findAll();
+	System.out.println("\nRestantes:");
+	for (UsuarioSistema _usuario : usuarios) {
+	    System.out.println(_usuario);
+	}
 	
 
 	System.out.println("\nListo!");
