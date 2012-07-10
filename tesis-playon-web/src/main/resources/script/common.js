5469e2fbd6cb94e2f351e36041cd7fed499c2c16
morales.batovski <morales.batovski@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-10 02:16:27 +0000

    Archivos movidos a otra cxarpeta de configuracion.

--- tesis-playon-web/src/main/resources/script/common.js
+++
@@ -1,6 +0,0 @@
-function changeSelectValue(selectNode, value){
-	for(var index=0;index<selectNode.length;index++){
-		if(selectNode[index].value == value)
-			selectNode.selectedIndex = index;
-	}	
-}
