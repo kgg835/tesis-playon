f0d434cfc286380e264d259319914f8fd0495be6
morales.batovski@gmail.com <morales.batovski@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-10 19:44:39 +0000

    Configuracion de spring.

--- tesis-playon-web/src/main/java/tesis/playon/common/controller/RolUsuarioController.java
+++ tesis-playon-web/src/main/java/tesis/playon/common/controller/RolUsuarioController.java
@@ -2,86 +2,100 @@ package tesis.playon.common.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import tesis.playon.common.validator.RolUsuarioValidator;
-import tesis.playon.web.business_object.dao.impl.RolUsuarioDao;
+import tesis.playon.web.business_object.impl.RolUsuarioBo;
 import tesis.playon.web.model.RolUsuario;
 
+/**
+ * 
+ * @author gmorales
+ * 
+ */
 @Controller
-@RequestMapping(value = "/registracionRolUsuario.htm")
+@RequestMapping(value = "/registracionRolUsuario")
 public class RolUsuarioController {
 
-    private RolUsuarioValidator validator = null;
-    private RolUsuarioDao rolUsuarioDao = null;
+    protected static Logger logger = Logger.getLogger("RolUsuarioController");
 
-    @Autowired
-    public void setRolUsuarioDao(RolUsuarioDao rolUsuarioDao) {
-	this.rolUsuarioDao = rolUsuarioDao;
+    @Resource(name = "rolUsuarioBo")
+    private RolUsuarioBo rolUsuarioBo = null;
+
+    @RequestMapping(value = "/rolesUsuario", method = RequestMethod.GET)
+    public String getRolUsuarios(Model model) {
+	logger.debug("Recibida peticion para mostrar todos los roles de usuarios");
+	List<RolUsuario> rolesUsuario = rolUsuarioBo.findAll();
+	model.addAttribute("rolesUsuario", rolesUsuario);
+	// Resuelve /WEB-INF/views/personspage.jsp
+	return "personspage";
     }
 
-    public RolUsuarioValidator getValidator() {
-	return validator;
+    @RequestMapping(value = "/rolesUsuario/add", method = RequestMethod.GET)
+    public String getAdd(Model model) {
+	logger.debug("Recibida peticion para mostrar pagina agregar");
+	// Create new Person and add to model
+	// This is the formBackingOBject
+	model.addAttribute("rolUsuarioAtributo", new RolUsuario());
+	// Resuelve /WEB-INF/views/addpage.jsp
+	return "addpage";
     }
 
-    @Autowired
-    public void setValidator(RolUsuarioValidator validator) {
-	this.validator = validator;
+    @RequestMapping(value = "/rolesUsuario/add", method = RequestMethod.POST)
+    public String add(@ModelAttribute("rolUsuarioAtributo") RolUsuario rolUsuario) {
+	logger.debug("Recibido pedido para agregar un rol de usuario");
+	// The "personAttribute" model has been passed to the controller from the JSP
+	// We use the name "personAttribute" because the JSP uses that name
+	// Call PersonService to do the actual adding
+	rolUsuarioBo.save(rolUsuario);
+	// resuelve /WEB-INF/views/addedpage.jsp
+	return "addedpage";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String showForm(ModelMap model) {
-	List<RolUsuario> listaRolUsuarios = rolUsuarioDao.findAll();
-	model.addAttribute("listaRolUsuarios", listaRolUsuarios);
-	RolUsuario rolUsuario = new RolUsuario();
-	model.addAttribute("rolUsuario", rolUsuario);
-	return "registracionRolUsuario";
+    @RequestMapping(value = "/rolesUsuario/delete", method = RequestMethod.GET)
+    public String delete(@RequestParam(value = "id", required = true) String nombreRol, Model model) {
+	logger.debug("Recibida la peticion para borra un rol de usuario existente");
+	// Call PersonService to do the actual deleting
+	RolUsuario rolUsuario = new RolUsuario(nombreRol);
+	rolUsuarioBo.delete(rolUsuario);
+	// Add id reference to Model
+	model.addAttribute("nombreRol", nombreRol);
+	// Resuelve /WEB-INF/views/deletedpage.jsp
+	return "deletedpage";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ModelAndView add(@ModelAttribute(value = "rolUsuario") RolUsuario rolUsuario, BindingResult result) {
-	validator.validate(rolUsuario, result);
-	ModelAndView mv = new ModelAndView("registracionRolUsuario");
-	if (!result.hasErrors()) {
-	    rolUsuarioDao.save(rolUsuario);
-	    rolUsuario = new RolUsuario();
-	    mv.addObject("rolUsuario", rolUsuario);
-	}
-	mv.addObject("listaRolUsuarios", rolUsuarioDao.findAll());
-	return mv;
+    @RequestMapping(value = "/rolesUsuario/edit", method = RequestMethod.GET)
+    public String getEdit(@RequestParam(value = "id", required = true) String nombreRol, Model model) {
+	logger.debug("Recibida la peticion para mostrar la pagina de edicion");
+	// Retrieve existing Person and add to model
+	// This is the formBackingOBject
+	model.addAttribute("rolUsuarioAtributo", rolUsuarioBo.findByNombreRolUsuario(nombreRol));
+	// Resuelve /WEB-INF/views/editpage.jsp
+	return "editpage";
     }
 
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public ModelAndView update(@ModelAttribute(value = "rolUsuario") RolUsuario rolUsuario, BindingResult result) {
-	validator.validate(rolUsuario, result);
-	ModelAndView mv = new ModelAndView("registracionRolUsuario");
-	if (!result.hasErrors()) {
-	    rolUsuarioDao.update(rolUsuario);
-	    rolUsuario = new RolUsuario();
-	    mv.addObject("rolUsuario", rolUsuario);
-	}
-	mv.addObject("listaRolUsuarios", rolUsuarioDao.findAll());
-	return mv;
-    }
-
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public ModelAndView delete(@ModelAttribute(value = "rolUsuario") RolUsuario rolUsuario, BindingResult result) {
-	validator.validate(rolUsuario, result);
-	ModelAndView mv = new ModelAndView("registracionRolUsuario");
-	if (!result.hasErrors()) {
-	    rolUsuarioDao.delete(rolUsuario);
-	    rolUsuario = new RolUsuario();
-	    mv.addObject("rolUsuario", rolUsuario);
-	}
-	mv.addObject("listaRolUsuarios", rolUsuarioDao.findAll());
-	return mv;
+    @RequestMapping(value = "/rolesUsuario/edit", method = RequestMethod.POST)
+    public String saveEdit(@ModelAttribute("rolUsuarioAtributo") RolUsuario rolUsuario,
+	    @RequestParam(value = "id", required = true) String nombreRol, Model model) {
+	logger.debug("Received request to update person");
+	// The "personAttribute" model has been passed to the controller from the JSP
+	// We use the name "personAttribute" because the JSP uses that name
+	// We manually assign the id because we disabled it in the JSP page
+	// When a field is disabled it will not be included in the ModelAttribute
+	rolUsuario.setNombre(nombreRol);
+	// Delegate to PersonService for editing
+	rolUsuarioBo.save(rolUsuario);
+	// Add id reference to Model
+	model.addAttribute("id", nombreRol);
+	// This will resolve to /WEB-INF/views/editedpage.jsp
+	return "editedpage";
     }
 
 }
