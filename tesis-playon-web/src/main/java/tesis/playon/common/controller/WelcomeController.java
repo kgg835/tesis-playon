70575bf39d7e756bab32f190cd72b1b15afa69ba
morales.batovski@gmail.com <morales.batovski@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-12 20:45:10 +0000

    Pagina de roles de usuario.

---
+++ tesis-playon-web/src/main/java/tesis/playon/common/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package tesis.playon.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+	model.addAttribute("message", "Bienvenido a \"PLAYÓN: Red de Playas\"");
+	return "welcome";
+    }
+}
