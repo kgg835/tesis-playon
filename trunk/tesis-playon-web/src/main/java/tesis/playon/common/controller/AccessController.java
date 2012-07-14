f7299f4fa6438a2acd75fbb9869f3f3485c6c61d
pablo.moreno.la31 <pablo.moreno.la31@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-14 23:04:19 +0000

    Primera idea de modulo de seguridad.

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/common/controller/AccessController.java
@@ -0,0 +1,34 @@
+package tesis.playon.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping
+public class AccessController {
+
+    @RequestMapping("/login")
+    public String login(Model model, @RequestParam(required = false) String message) {
+	model.addAttribute("message", message);
+	return "access/login";
+    }
+
+    @RequestMapping(value = "/denied")
+    public String denied() {
+	return "access/denied";
+    }
+
+    @RequestMapping(value = "/login/failure")
+    public String loginFailure() {
+	String message = "¡Usuario y/o contraseña incorrectos!";
+	return "redirect:/login?message=" + message;
+    }
+
+    @RequestMapping(value = "/logout/success")
+    public String logoutSuccess() {
+	String message = "¡Accesso logrado!";
+	return "redirect:/login?message=" + message;
+    }
+}
