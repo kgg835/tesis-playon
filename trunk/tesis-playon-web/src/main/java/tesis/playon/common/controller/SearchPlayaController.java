ca20c8e0d8ce154673eab833ec6dd7762dcc1af4
pablo.moreno.la31 <pablo.moreno.la31@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-18 08:09:05 +0000

    Busqueda en gmap de un domicilio!!

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/common/controller/SearchPlayaController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package tesis.playon.common.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import tesis.playon.web.model.Empleado;
+
+/**
+ * @author Pablo
+ *
+ */
+@Controller
+@RequestMapping(value = "/searchplayas")
+public class SearchPlayaController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getPlayas(Model model) {
+	return "searchplayas";
+    }
+}
