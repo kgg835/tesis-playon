8fcd2bb54496593da1316cb7d93826090438fdea
morales.batovski@gmail.com <morales.batovski@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-20 15:25:38 +0000

    Service de tipo documento agregado.

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/service/impl/TipoDocService.java
@@ -0,0 +1,52 @@
+package tesis.playon.web.service.impl;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import tesis.playon.web.dao.ITipoDocDao;
+import tesis.playon.web.model.TipoDoc;
+import tesis.playon.web.service.ITipoDocService;
+
+@Transactional(readOnly = true)
+public class TipoDocService implements ITipoDocService {
+
+    ITipoDocDao tipoDocDao;
+
+    @Transactional(readOnly = false)
+    @Override
+    public void save(TipoDoc tipoDoc) {
+	getTipoDocDao().save(tipoDoc);
+    }
+
+    @Transactional(readOnly = false)
+    @Override
+    public void update(TipoDoc tipoDoc) {
+	getTipoDocDao().update(tipoDoc);
+    }
+
+    @Transactional(readOnly = false)
+    @Override
+    public void delete(TipoDoc tipoDoc) {
+	getTipoDocDao().delete(tipoDoc);
+    }
+
+    @Override
+    public List<TipoDoc> findAll() {
+	return getTipoDocDao().findAll();
+    }
+
+    @Override
+    public TipoDoc findByNombreTipoDoc(String nombreTipoDoc) {
+	return getTipoDocDao().findByNombreTipoDoc(nombreTipoDoc);
+    }
+
+    public ITipoDocDao getTipoDocDao() {
+	return tipoDocDao;
+    }
+
+    public void setTipoDocDao(ITipoDocDao tipoDocDao) {
+	this.tipoDocDao = tipoDocDao;
+    }
+
+}
