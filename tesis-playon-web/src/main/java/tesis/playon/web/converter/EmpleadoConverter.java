f5452e208a0402ef24a82b5f2e2b4c053ea21cfa
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-07-26 23:40:29 +0000

    empleado converter y estado playa converter

---
+++ tesis-playon-web/src/main/java/tesis/playon/web/converter/EmpleadoConverter.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package tesis.playon.web.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import tesis.playon.web.model.Empleado;
+
+/**
+ * @author Pablo
+ *
+ */
+@FacesConverter(value = "playaConverter")
+public class EmpleadoConverter implements Converter{
+    
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+    String toObject[] = value.split(":");
+	Empleado empleado = new Empleado();
+	empleado.setId(Integer.parseInt(toObject[0]));
+	return empleado;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+	if (value instanceof Empleado) {
+	    Empleado empleado = (Empleado) value;
+	    String idEmpleado = Integer.toString(empleado.getId());
+	    String toString = idEmpleado;
+	    return toString;
+	} else {
+	    return "No se pudo parsear el objeto.";
+	}
+    }
+}
---
+++ tesis-playon-web/src/main/java/tesis/playon/web/converter/EstadoPlayaConverter.java
@@ -0,0 +1,34 @@
+package tesis.playon.web.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import tesis.playon.web.model.EstadoPlaya;
+
+@FacesConverter(value = "tipoDocConverter")
+public class EstadoPlayaConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+	String toObject[] = value.split(":");
+	EstadoPlaya estado = new EstadoPlaya();
+	estado.setId(Integer.parseInt(toObject[0]));
+	estado.setNombre(toObject[1]);
+	return estado;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+	if (value instanceof EstadoPlaya) {
+	    EstadoPlaya estado = (EstadoPlaya) value;
+	    String idEstado = Integer.toString(estado.getId());
+	    String nombreEstado = estado.getNombre();
+	    String toString = idEstado + ":" + nombreEstado;
+	    return toString;
+	} else {
+	    return "No se pudo parsear el objeto.";
+	}
+    }
+}
