b587615cc93d3c2b923f82e3d19ea30f92478e83
alebostico@gmail.com <alebostico@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-08-07 02:04:33 +0000

    correjido el error del orto!

--- tesis-playon-web/src/main/java/tesis/playon/web/model/Usuario.java
+++ tesis-playon-web/src/main/java/tesis/playon/web/model/Usuario.java
@@ -59,7 +59,7 @@ public class Usuario implements Serializable {
     @Column(name = "usuario", unique = true)
     private String nombreUser;
     
-    @Column(name = "enable", columnDefinition = "TINYINT default 1")
+    @Column(name = "enable", columnDefinition = "TINYINT")
     @Type(type = "org.hibernate.type.NumericBooleanType")
     private Boolean enable;
 
