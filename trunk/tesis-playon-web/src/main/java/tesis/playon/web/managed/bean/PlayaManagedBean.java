7f76d093c0f97f210f2678af80c55bafea88f08f
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-08-07 17:29:16 +0000

    Buscar playa

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/PlayaManagedBean.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/PlayaManagedBean.java
@@ -394,7 +394,7 @@ public class PlayaManagedBean implements Serializable {
 	    playaResultadoBusqueda = new ArrayList<Playa>();
 	    for (Playa playaAux : getPlayaList()) {
 		Double comparacion = playaAux.getDistanceFrom(respuesta.getLatitud(), respuesta.getLongitud());
-		if (comparacion < getDistancia() && playaAux.getEstado().getId()!=1) {
+		if (comparacion < getDistancia() && playaAux.getEstado().getId()==2) {
 		    playaResultadoBusqueda.add(playaAux);
 		    LatLng coord1 = new LatLng(playaAux.getLatitud(), playaAux.getLongitud());
 		    advancedModel.addOverlay(new Marker(coord1, playaAux.toString2(), null,
