1e9a5e38785baec75484d6b02d84da86bbb71326
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-08-12 07:17:34 +0000

    Enlazar un combo!!!

--- tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/PaisManagedBean.java
+++ tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/PaisManagedBean.java
@@ -1,19 +1,18 @@
 package tesis.playon.web.managed.bean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 import tesis.playon.web.model.Barrio;
 import tesis.playon.web.model.Localidad;
 import tesis.playon.web.model.Pais;
 import tesis.playon.web.model.Provincia;
-import tesis.playon.web.service.IBarrioService;
-import tesis.playon.web.service.ILocalidadService;
 import tesis.playon.web.service.IPaisService;
 import tesis.playon.web.service.IProvinciaService;
 
@@ -23,14 +22,14 @@ import tesis.playon.web.service.IProvinciaService;
  * 
  */
 @ManagedBean(name = "paisMB")
-@RequestScoped
+@ViewScoped
 public class PaisManagedBean implements Serializable {
 
     private static final long serialVersionUID = -1085389423375986168L;
 
-    //private static final String LISTA_PAIS = "paislist";
+    // private static final String LISTA_PAIS = "paislist";
 
-    //private static final String ERROR = "error";
+    // private static final String ERROR = "error";
 
     @ManagedProperty(value = "#{PaisService}")
     IPaisService paisService;
@@ -38,141 +37,101 @@ public class PaisManagedBean implements Serializable {
     @ManagedProperty(value = "#{ProvinciaService}")
     IProvinciaService provinciaService;
 
-    @ManagedProperty(value = "#{LocalidadService}")
-    ILocalidadService localidadService;
+    private Map<String, Pais> paises = new HashMap<String, Pais>();
 
-    @ManagedProperty(value = "#{BarrioService}")
-    IBarrioService barrioService;
+    private Map<String, Provincia> provincias = new HashMap<String, Provincia>();
 
-    List<Pais> paisList;
-    
-    private Pais pais;
-    
-    private Provincia provincia;
-    
+    private Map<String, Map<String, Provincia>> provinciasData = new HashMap<String, Map<String, Provincia>>();
+
+    private String pais;
+
+    private String provincia;
+
     private Localidad localidad;
-    
-    private Barrio barrio;
 
-    List<Provincia> provinciaList;
+    private Barrio barrio;
 
     List<Localidad> localidadList;
 
     List<Barrio> barrioList;
 
     public IPaisService getPaisService() {
-        return paisService;
+	return paisService;
     }
 
     public void setPaisService(IPaisService paisService) {
-        this.paisService = paisService;
+	this.paisService = paisService;
     }
 
     public IProvinciaService getProvinciaService() {
-        return provinciaService;
+	return provinciaService;
     }
 
     public void setProvinciaService(IProvinciaService provinciaService) {
-        this.provinciaService = provinciaService;
+	this.provinciaService = provinciaService;
     }
 
-    public ILocalidadService getLocalidadService() {
-        return localidadService;
+    public Map<String, Pais> getPaises() {
+	List<Pais> paisList = getPaisService().findAll();
+	for (Pais pais : paisList) {
+	    System.out.println(pais.getNombre());
+	    paises.put(pais.getNombre(), pais);
+
+	    Map<String, Provincia> prov = new HashMap<String, Provincia>();
+	    List<Provincia> provinciaList = getProvinciaService().findProvincias(pais);
+	    for (Provincia provincia : provinciaList) {
+		System.out.println(provincia.getNombre());
+		prov.put(provincia.getNombre(), provincia);
+	    }
+	    provinciasData.put(pais.getNombre(), prov);
+	}
+	return paises;
     }
 
-    public void setLocalidadService(ILocalidadService localidadService) {
-        this.localidadService = localidadService;
+    public void setPaises(Map<String, Pais> paises) {
+	this.paises = paises;
     }
 
-    public IBarrioService getBarrioService() {
-        return barrioService;
+    public Map<String, Provincia> getProvincias() {
+	return provincias;
     }
 
-    public void setBarrioService(IBarrioService barrioService) {
-        this.barrioService = barrioService;
+    public Map<String, Map<String, Provincia>> getProvinciasData() {
+	return provinciasData;
     }
 
-    public List<Pais> getPaisList() {
-	paisList = new ArrayList<Pais>();
-	paisList.addAll(getPaisService().findAll());
-	return paisList;
+    public void setProvinciasData(Map<String, Map<String, Provincia>> provinciasData) {
+	this.provinciasData = provinciasData;
     }
 
-    public void setPaisList(List<Pais> paisList) {
-        this.paisList = paisList;
+    public void setProvincias(Map<String, Provincia> provincias) {
+	this.provincias = provincias;
     }
 
-    public Pais getPais() {
+    public String getPais() {
         return pais;
     }
 
-    public void setPais(Pais pais) {
+    public void setPais(String pais) {
         this.pais = pais;
     }
 
-    public Provincia getProvincia() {
+    public String getProvincia() {
         return provincia;
     }
 
-    public void setProvincia(Provincia provincia) {
+    public void setProvincia(String provincia) {
         this.provincia = provincia;
     }
 
-    public Localidad getLocalidad() {
-        return localidad;
-    }
-
-    public void setLocalidad(Localidad localidad) {
-        this.localidad = localidad;
-    }
-
-    public Barrio getBarrio() {
-        return barrio;
-    }
-
-    public void setBarrio(Barrio barrio) {
-        this.barrio = barrio;
-    }
-
-    public List<Provincia> getProvinciaList() {
-	if(pais != null){
-	    provinciaList = getProvinciaService().findProvincias(pais);
-	}
-	else
-	    provinciaList = new ArrayList<Provincia>();
-	return provinciaList;
-    }
-
-    public void setProvinciaList(List<Provincia> provinciaList) {
-        this.provinciaList = provinciaList;
-    }
-
-    public List<Localidad> getLocalidadList() {
-        return localidadList;
-    }
-
-    public void setLocalidadList(List<Localidad> localidadList) {
-        this.localidadList = localidadList;
-    }
-
-    public List<Barrio> getBarrioList() {
-        return barrioList;
-    }
-
-    public void setBarrioList(List<Barrio> barrioList) {
-        this.barrioList = barrioList;
-    }
-    
     public void handlePaisChange() {
-	getLocalidadList();
+	if (pais != null && !pais.equals("")) {
+	    System.out.println("EL PAIS VALIA ALGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+	    System.out.println("PAIS: " + pais);
+	    provincias = provinciasData.get(pais);
+	} else {
+	    System.out.println("EL PAIS ERA NULOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+	    provincias = new HashMap<String, Provincia>();
+	}
     }
-    
-//    public void handleProvinciaChange() {
-//	if(provinciaId != 0){
-//	    Provincia provincia = getProvinciaService().findByProvinciaId(provinciaId);
-//	    localidadList = getLocalidadService().find
-//	}
-//	else
-//	    provinciaList = new ArrayList<Provincia>();
-//    }
 }
