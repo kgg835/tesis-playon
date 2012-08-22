0cab839472ce52e3478b94b3454ae26509941f57
morales.batovski <morales.batovski@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-08-22 00:05:11 +0000

    Estadias creadas para las playas existentes.

--- trunk/tesis-playon-db/db_script_structure+basicdata+user.sql
+++ trunk/tesis-playon-db/db_script_structure+basicdata+user.sql
@@ -996,6 +996,14 @@ CREATE TABLE `estadia` (
 
 LOCK TABLES `estadia` WRITE;
 /*!40000 ALTER TABLE `estadia` DISABLE KEYS */;
+INSERT INTO `estadia` (`estadiaID`, `playaID`) 
+VALUES ('3','3'),
+	('4','4'),
+	('5','5'),
+	('6','6'),
+	('7','7'),
+	('8','8'),
+	('9','9');
 /*!40000 ALTER TABLE `estadia` ENABLE KEYS */;
 UNLOCK TABLES;
 
