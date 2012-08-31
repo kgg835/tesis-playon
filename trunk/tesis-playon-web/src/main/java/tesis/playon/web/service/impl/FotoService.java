b712f95aa923742cae851ef192d111155e046291
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-08-31 18:57:15 +0000

    FotoService terminado.. falta agregar en el applicationContext.

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/service/impl/FotoService.java
@@ -0,0 +1,66 @@
+package tesis.playon.web.service.impl;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import tesis.playon.web.dao.IFotoDao;
+import tesis.playon.web.model.Foto;
+import tesis.playon.web.service.IFotoService;
+/**
+ * 
+ * @author pablo
+ *
+ */
+@Transactional(readOnly = true)
+public class FotoService implements IFotoService {
+    
+    IFotoDao fotoDao;
+    
+    @Transactional(readOnly = false)
+    @Override
+    public void save(Foto foto) {
+	// TODO Auto-generated method stub
+	getFotoDao().save(foto);
+    }
+
+    @Transactional(readOnly = false)
+    @Override
+    public void update(Foto foto) {
+	// TODO Auto-generated method stub
+	getFotoDao().update(foto);
+    }
+
+    @Transactional(readOnly = false)
+    @Override
+    public void delete(Foto foto) {
+	// TODO Auto-generated method stub
+	getFotoDao().delete(foto);
+    }
+
+    @Override
+    public Foto findByLinkFoto(String link) {
+	// TODO Auto-generated method stub
+	return getFotoDao().findByLinkFoto(link);
+    }
+
+    @Override
+    public List<Foto> findAll() {
+	// TODO Auto-generated method stub
+	return getFotoDao().findAll();
+    }
+
+    @Override
+    public Integer obtenerUltimoID() {
+	// TODO Auto-generated method stub
+	return getFotoDao().obtenerUltimoID();
+    }
+
+    public IFotoDao getFotoDao() {
+        return fotoDao;
+    }
+
+    public void setFotoDao(IFotoDao fotoDao) {
+        this.fotoDao = fotoDao;
+    }
+}
