1e112e178ddac94265cb1eeedb858c602e76f138
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-09-03 04:18:43 +0000

    Mail funcionando. En principio para el formulario de contacto y para cuando se crea un cliente. Faltaria ver el tema de habilitar al cliente y demas, pero el envio funciona bien.

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/contactoMB.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/contactoMB.java
@@ -6,31 +6,95 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import tesis.playon.web.model.Mail;
+import tesis.playon.web.util.NotificadorUtil;
 
 @ManagedBean(name = "contactoMB")
 @RequestScoped
-public class contactoMB implements Serializable{
+public class contactoMB implements Serializable {
 
     private static final long serialVersionUID = -1085389423375986168L;
-    
-    private Mail mail;
-    private MailManagedBean mailMB;
+
+    private String nombre;
+    private String asuntoContacto;
+    private String mensaje;
+    private String destinatario;
+    private String mailContacto;
+    private Mail mail = new Mail();
+
+    // private MailManagedBean mailMB=new MailManagedBean();
+    private NotificadorUtil notificador;
 
     public void enviarMailContacto()
 
     {
-	mail.setDatosContacto();
+	mensaje = "\nDatos de contacto: " + "\nNombre: " + getNombre() + "\nE-Mail: " + getMailContacto()
+		+ "\nAsunto: " + getAsuntoContacto() + "\n\n\nMENSAJE: " + getMensaje();
+	notificador = new NotificadorUtil();
 	mail.setDestinatario("tesisplayon@gmail.com");
-	mailMB.enviar(mail);
+	mail.setMensaje(getMensaje());
+	mail.setAsunto(getAsuntoContacto());
+
+	notificador.enviar(getMail());
+	reset();
 
     }
 
+    public void reset()
+    {
+	
+	this.mensaje="";
+	this.nombre="";
+	this.asuntoContacto="";
+	this.mailContacto="";
+	
+    }
+    
     public Mail getMail() {
 	return mail;
     }
 
     public void setMail(Mail mail) {
 	this.mail = mail;
+    }
+
+    public String getNombre() {
+	return nombre;
+    }
+
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
+
+    public String getAsuntoContacto() {
+	return asuntoContacto;
+    }
+
+    public void setAsuntoContacto(String asuntoContacto) {
+	this.asuntoContacto = asuntoContacto;
+    }
+
+    public String getMensaje() {
+	return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+	this.mensaje = mensaje;
+    }
+
+    public String getDestinatario() {
+	return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+	this.destinatario = destinatario;
+    }
+
+    public String getMailContacto() {
+	return mailContacto;
+    }
+
+    public void setMailContacto(String mailContacto) {
+	this.mailContacto = mailContacto;
     }
 
 }
