a2a0f5423ea90fad98bd8c95ef0bcac52357b49e
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-10-04 20:19:12 +0000

    Nueva Promoción funcionando correctamente.
    *NOTA: Ver los estados de las promociones.

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/PromocionManagedBean.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/PromocionManagedBean.java
@@ -4,14 +4,21 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import tesis.playon.web.model.EstadoPromocion;
 import tesis.playon.web.model.Playa;
+import tesis.playon.web.model.Promocion;
 import tesis.playon.web.model.Tarifa;
+import tesis.playon.web.model.Usuario;
 import tesis.playon.web.service.IEstadoPromocionService;
 import tesis.playon.web.service.IPlayaService;
+import tesis.playon.web.service.IPromocionService;
 import tesis.playon.web.service.ITarifaService;
 import tesis.playon.web.service.IUsuarioService;
 
@@ -27,6 +34,9 @@ public class PromocionManagedBean implements Serializable {
 
     @ManagedProperty(value = "#{EstadoPromocionService}")
     IEstadoPromocionService estadoPromocionService;
+
+    @ManagedProperty(value = "#{PromocionService}")
+    IPromocionService promocionService;
 
     @ManagedProperty(value = "#{PlayaService}")
     IPlayaService playaService;
@@ -51,29 +61,97 @@ public class PromocionManagedBean implements Serializable {
 
     private Float montoFijo;
 
+    private Float montoConDescuento;
+
     private String nombre;
 
-    private Tarifa tarifa;
+    private static Tarifa tarifa;
 
     private Playa playa;
 
     private EstadoPromocion estadoPromocion;
+    
+    private Date today;
 
     @PostConstruct
     private void init() {
+	FacesContext facesContext = FacesContext.getCurrentInstance();
+	String userName = facesContext.getExternalContext().getRemoteUser();
+	Usuario user = getUsuarioService().findByNombreUsuario(userName);
+	if (user != null && user.getPlaya() != null) {
+	    this.playa = user.getPlaya();
+	}
+	today= new Date();
+    }
 
-    }
-    
-    public String addPromocion(){
+    public String addPromocion() {
+	Promocion promocion = null;
+	
+	try {
+	    promocion = new Promocion();
+	    promocion.setDescripcion(getDescripcion());
+	    promocion.setDescuento(getDescuento());
+	    promocion.setEstadoPromocion(getEstadoPromocion());
+	    promocion.setFechaAlta(new Date());
+	    promocion.setFechaFin(getFechaFin());
+	    promocion.setFechaInicio(getFechaInicio());
+	    promocion.setMontoFijo(getTarifa().getImporte());
+	    promocion.setNombre(getNombre());
+	    promocion.setPlaya(getPlaya());
+	    promocion.setTarifa(getTarifa());
+	    
+	    getPromocionService().save(promocion);
+
+	    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+		    "Se registró exitosamente la promoción: ", promocion.getNombre());
+	    FacesContext.getCurrentInstance().addMessage(null, message);
+
+	    return "promocionaddend";
+	}
+	catch (Exception e) {
+	    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+		    "Error, no se pudo registrar la promoción, Disculpe las molestias ocacionadas.", null);
+	    FacesContext.getCurrentInstance().addMessage(null, message);
+	    e.printStackTrace();
+	}
 	return null;
     }
-    
-    public String updatePromocion(){
+
+    public String updatePromocion() {
 	return null;
     }
-    
-    public String deletePromocion(){
+
+    public String deletePromocion() {
 	return null;
+    }
+
+    public void calcularDescuento() {
+	this.montoConDescuento = tarifa.getImporte() *(1- (descuento / 100));
+    }
+
+    public void seleccionarTarifa(Tarifa tarifaParameter) {
+	if (tarifaParameter != null) {
+	    tarifa = tarifaParameter;
+	    this.montoFijo = tarifaParameter.getImporte();
+	}
+    }
+
+    public void validateDateInicial(FacesContext context, UIComponent component, Object value) {
+	Date dateInicial = (Date) value;
+	if (dateInicial != null) {
+	    this.setFechaInicio(dateInicial);
+	}
+    }
+    
+    public void validateDate(FacesContext context, UIComponent component, Object value) {
+	Date dateFin = (Date) value;
+	if (getFechaInicio() != null) {
+	    if(dateFin.before(getFechaInicio())){
+	    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+		    "La fecha de finalización debe ser mayor a la fecha de inicio.", null);
+	    throw new ValidatorException(message);
+	    }
+	}
     }
 
     public IUsuarioService getUsuarioService() {
@@ -109,6 +187,14 @@ public class PromocionManagedBean implements Serializable {
     }
 
     // ============================ GETTER && SETTER =======================================
+
+    public IPromocionService getPromocionService() {
+	return promocionService;
+    }
+
+    public void setPromocionService(IPromocionService promocionService) {
+	this.promocionService = promocionService;
+    }
 
     public String getDescripcion() {
 	return descripcion;
@@ -171,7 +257,7 @@ public class PromocionManagedBean implements Serializable {
     }
 
     public void setTarifa(Tarifa tarifa) {
-	this.tarifa = tarifa;
+	PromocionManagedBean.tarifa = tarifa;
     }
 
     public Playa getPlaya() {
@@ -190,4 +276,19 @@ public class PromocionManagedBean implements Serializable {
 	this.estadoPromocion = estadoPromocion;
     }
 
+    public Float getMontoConDescuento() {
+	return montoConDescuento;
+    }
+
+    public void setMontoConDescuento(Float montoConDescuento) {
+	this.montoConDescuento = montoConDescuento;
+    }
+
+    public Date getToday() {
+        return today;
+    }
+
+    public void setToday(Date today) {
+        this.today = today;
+    }
 }
