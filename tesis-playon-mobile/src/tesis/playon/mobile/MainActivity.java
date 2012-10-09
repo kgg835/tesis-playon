0daf89e29da398ef4c0c3d10bbd4fd37efcc0ea4
ericperezvillar@gmail.com <ericperezvillar@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-10-09 23:50:23 +0000

    Pantalla para login y consumo de servicio.

--- tesis-playon-mobile/src/tesis/playon/mobile/MainActivity.java
+++ tesis-playon-mobile/src/tesis/playon/mobile/MainActivity.java
@@ -1,25 +1,46 @@
 package tesis.playon.mobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    private final int LOGIN_ID = 50001;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+
+	// llamo a la actividad para loguearse
+	Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+	startActivityForResult(loginIntent, LOGIN_ID);
     }
-    
+
+    // @Override
+    // public boolean onCreateOptionsMenu(Menu menu) {
+    // getMenuInflater().inflate(R.menu.activity_main, menu);
+    // return true;
+    // }
+
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	super.onActivityResult(requestCode, resultCode, data);
+	try {
+	    switch (requestCode) {
+		case LOGIN_ID: {
+		    if (resultCode == Activity.RESULT_OK) {
+			Toast.makeText(getBaseContext(), "Logueado correctamente", Toast.LENGTH_SHORT).show();
+		    } else {
+			Toast.makeText(getBaseContext(), "El usuario no existe", Toast.LENGTH_SHORT).show();
+		    }
+		    break;
+		}
+	    }
+	} catch (Exception e) {
+	    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+	}
     }
 }
