50f6a658568dc273f8a43d0e30fb31fb319d6318
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-10-12 23:42:39 +0000

    Servicios para denuncias de vehiculos

---
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/service/impl/DenunciaVehiculoService.java
@@ -0,0 +1,57 @@
+package tesis.playon.web.service.impl;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import tesis.playon.web.dao.IDenunciaVehiculoDao;
+import tesis.playon.web.model.DenunciaVehiculo;
+import tesis.playon.web.service.IDenunciaVehiculoService;
+
+/**
+ * 
+ * @author pablo
+ * 
+ */
+@Transactional(readOnly = true)
+public class DenunciaVehiculoService implements IDenunciaVehiculoService {
+
+    IDenunciaVehiculoDao denunciaVehiculoDao;
+
+    @Transactional(readOnly = false)
+    @Override
+    public void save(DenunciaVehiculo denuncia) {
+	getDenunciaVehiculoDao().save(denuncia);
+    }
+
+    @Transactional(readOnly = false)
+    @Override
+    public void update(DenunciaVehiculo denuncia) {
+	getDenunciaVehiculoDao().update(denuncia);
+    }
+
+    @Transactional(readOnly = false)
+    @Override
+    public void delete(DenunciaVehiculo denuncia) {
+	getDenunciaVehiculoDao().delete(denuncia);
+    }
+
+    @Override
+    public DenunciaVehiculo findByAsuntoDenunciaVehiculo(String asuntoDenunciaVehiculo) {
+	return getDenunciaVehiculoDao().findByAsuntoDenunciaVehiculo(asuntoDenunciaVehiculo);
+    }
+
+    @Override
+    public List<DenunciaVehiculo> findAll() {
+	return getDenunciaVehiculoDao().findAll();
+    }
+
+    public IDenunciaVehiculoDao getDenunciaVehiculoDao() {
+	return denunciaVehiculoDao;
+    }
+
+    public void setDenunciaVehiculoDao(IDenunciaVehiculoDao denuncia) {
+	this.denunciaVehiculoDao = denuncia;
+    }
+
+}
