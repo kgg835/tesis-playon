207f4ad5bb29af734a9c547e09d8452ee8e010eb
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2012-10-30 03:02:49 +0000

    le cambie el estado a una playa para que este pendiente

--- tesis-playon-db/db_script_structure+basicdata+user.sql
+++ tesis-playon-db/db_script_structure+basicdata+user.sql
@@ -1233,7 +1233,7 @@ VALUES (2,'30-11111111-1',55,'Buenos Aires 548',2,'Estacionamiento Europa',-31.4
 	(2,'30-11111111-1',100,'Independencia 770',2,'Estacionamiento Independencia',-31.425024,-64.188105,'Estacionamiento Independencia',4),
 	(2,'30-11111111-1',100,'Obispo Trejo 758',2,'Parkingcity',-31.424615,-64.189357,'Parkingcity',5),
 	(1,'30-11111111-1',100,'Bv. Ilia 156',2,'Estacionamiento Mercado Sur',-31.421251,-64.184234,'Estacionamiento Mercado Sur',6),
-	(1,'30-11111111-1',100,'Santa Rosa 631',2,'Parking One',-31.409921,-64.191527,'Parking One',7),
+	(1,'30-11111111-1',100,'Santa Rosa 631',1,'Parking One',-31.409921,-64.191527,'Parking One',7),
 	(1,'30-11111111-1',100,'Bv. Chacabuco 728',2,'Playa Sur',-31.425904,-64.183791,'Playa Sur',8),
 	(1,'30-11111111-1',100,'Bv. Illia 70',2,'Parquin Verde',-31.420956,-64.18535,'Parquin Verde',9),
 	(1,'30-11111111-1',100,'Colon 756',2,'Estacionamiento Colón',-31.4105156,-64.1936078,'Estacionamiento Colón',10);
