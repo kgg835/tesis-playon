629789944f1ca61c43b7d6ff64564ff7c341a7a6
morales.batovski <morales.batovski@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-01-19 00:31:28 +0000

    Resultados de playas por lista funcionando de nuevo

--- tesis-playon-mobile/src/tesis/playon/mobile/BuscarPlayasActivity.java
+++ tesis-playon-mobile/src/tesis/playon/mobile/BuscarPlayasActivity.java
@@ -41,7 +41,7 @@ public class BuscarPlayasActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+	setContentView(R.layout.lista_playas);
 
 	handleIntent(getIntent());
     }
