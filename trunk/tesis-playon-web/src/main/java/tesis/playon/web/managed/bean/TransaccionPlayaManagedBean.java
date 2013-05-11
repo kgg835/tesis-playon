20773a7c61ad163d0868e1685d024368987298cf
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-05-11 21:50:29 +0000

    arreglos en vista

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/TransaccionPlayaManagedBean.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/TransaccionPlayaManagedBean.java
@@ -49,266 +49,286 @@ import com.lowagie.text.Paragraph;
 @ViewScoped
 public class TransaccionPlayaManagedBean implements Serializable {
 
-    private static final long serialVersionUID = 3260243859983118685L;
+	private static final long serialVersionUID = 3260243859983118685L;
 
-    @ManagedProperty(value = "#{TransaccionPlayaService}")
-    private ITransaccionPlayaService transaccionPlayaService;
+	@ManagedProperty(value = "#{TransaccionPlayaService}")
+	private ITransaccionPlayaService transaccionPlayaService;
 
-    @ManagedProperty(value = "#{PlayaService}")
-    private IPlayaService playaService;
+	@ManagedProperty(value = "#{PlayaService}")
+	private IPlayaService playaService;
 
-    @ManagedProperty(value = "#{CuentaPlayaService}")
-    private ICuentaPlayaService cuentaPlayaService;
+	@ManagedProperty(value = "#{CuentaPlayaService}")
+	private ICuentaPlayaService cuentaPlayaService;
 
-    @ManagedProperty(value = "#{TipoPagoService}")
-    private ITipoPagoService tipoPagoService;
+	@ManagedProperty(value = "#{TipoPagoService}")
+	private ITipoPagoService tipoPagoService;
 
-    private static List<TransaccionPlaya> transaccionesList;
+	private static List<TransaccionPlaya> transaccionesList;
 
-    private static List<TransaccionPlaya> transaccionesPorFechaList;
+	private static List<TransaccionPlaya> transaccionesPorFechaList;
 
-    private static Liquidacion liquidacionSelected;
+	private static Liquidacion liquidacionSelected;
 
-    private static DetalleEstadia estadiaSelected;
+	public static TransaccionPlaya transaccionSelected;
 
-    private String acTxtPlaya;
+	private String acTxtPlaya;
 
-    private Date fechaDesde;
+	private Date fechaDesde;
 
-    private Date fechaHasta;
+	private Date fechaHasta;
 
-    private SelectItem[] tipoPagoOptions;
+	private SelectItem[] tipoPagoOptions;
 
-    @PostConstruct
-    private void init() {
-	this.fechaDesde = new Date();
-	this.fechaHasta = new Date();
-	this.fechaDesde = DateUtils.setDays(this.fechaDesde, 1);
-	if (liquidacionSelected != null && transaccionesList == null) {
-	    updateTransaccionesList();
-	}
-    }
-
-    public List<String> complete(String query) {
-	List<String> results = new ArrayList<String>();
-
-	List<Playa> playas = this.getPlayaService().findByLikeNombreComercial(query);
-
-	for (Playa nombrePlaya : playas) {
-	    results.add(nombrePlaya.getNombreComercial());
+	@PostConstruct
+	private void init() {
+		this.fechaDesde = new Date();
+		this.fechaHasta = new Date();
+		this.fechaDesde = DateUtils.setDays(this.fechaDesde, 1);
+		if (liquidacionSelected != null && transaccionesList == null) {
+			updateTransaccionesList();
+		}
 	}
 
-	return results;
-    }
+	public List<String> complete(String query) {
+		List<String> results = new ArrayList<String>();
 
-    public List<Playa> completePlayas(String query) {
+		List<Playa> playas = this.getPlayaService().findByLikeNombreComercial(
+				query);
 
-	List<Playa> playas = this.getPlayaService().findByLikeNombreComercial(query);
+		for (Playa nombrePlaya : playas) {
+			results.add(nombrePlaya.getNombreComercial());
+		}
 
-	return playas;
-    }
-
-    public List<TransaccionPlaya> updateTransaccionesPorFechaList() {
-	Date fechaDesde = (this.fechaDesde != null ? this.fechaDesde : new Date(01012012));
-	Date fechaHasta = (this.fechaHasta != null ? this.fechaHasta : Calendar.getInstance().getTime());
-
-	Playa playa = getPlayaService().findByNombreComercial(this.acTxtPlaya);
-	CuentaPlaya cuentaPlaya = getCuentaPlayaService().findByPlaya(playa);
-
-	transaccionesPorFechaList = getTransaccionPlayaService().findTransaccionesByFecha(cuentaPlaya, fechaDesde,
-		fechaHasta);
-	return transaccionesPorFechaList;
-    }
-
-    public List<TransaccionPlaya> getTransaccionesPorFechaList() {
-
-	return transaccionesPorFechaList;
-    }
-
-    public void setTransaccionesPorFechaList(List<TransaccionPlaya> transaccionesPorFechaList) {
-	TransaccionPlayaManagedBean.transaccionesPorFechaList = transaccionesPorFechaList;
-    }
-
-    public void updateTransaccionesList() {
-	transaccionesList = getTransaccionPlayaService().findTransaccionesByLiquidacion(liquidacionSelected);
-    }
-
-    public List<TransaccionPlaya> getTransaccionesList() {
-	// transaccionesList =
-	// getTransaccionPlayaService().findTransaccionesByLiquidacion(liquidacionSelected);
-	if (transaccionesList == null) {
-	    updateTransaccionesList();
-	}
-	return transaccionesList;
-    }
-
-    public void setTransaccionesList(List<TransaccionPlaya> transaccionesList) {
-	TransaccionPlayaManagedBean.transaccionesList = transaccionesList;
-    }
-
-    public Liquidacion getLiquidacionSelected() {
-	return liquidacionSelected;
-    }
-
-    public void setLiquidacionSelected(Liquidacion liquidacionSelected) {
-	TransaccionPlayaManagedBean.liquidacionSelected = liquidacionSelected;
-    }
-
-    public ITransaccionPlayaService getTransaccionPlayaService() {
-	return transaccionPlayaService;
-    }
-
-    public void setTransaccionPlayaService(ITransaccionPlayaService transaccionPlayaService) {
-	this.transaccionPlayaService = transaccionPlayaService;
-    }
-
-    public IPlayaService getPlayaService() {
-	return playaService;
-    }
-
-    public void setPlayaService(IPlayaService playaService) {
-	this.playaService = playaService;
-    }
-
-    public ICuentaPlayaService getCuentaPlayaService() {
-	return cuentaPlayaService;
-    }
-
-    public void setCuentaPlayaService(ICuentaPlayaService cuentaPlayaService) {
-	this.cuentaPlayaService = cuentaPlayaService;
-    }
-
-    public ITipoPagoService getTipoPagoService() {
-	return tipoPagoService;
-    }
-
-    public void setTipoPagoService(ITipoPagoService tipoPagoService) {
-	this.tipoPagoService = tipoPagoService;
-    }
-
-    public String getAcTxtPlaya() {
-	return acTxtPlaya;
-    }
-
-    public void setAcTxtPlaya(String acTxtPlaya) {
-	this.acTxtPlaya = acTxtPlaya;
-    }
-
-    public Date getFechaDesde() {
-	return fechaDesde;
-    }
-
-    public void setFechaDesde(Date fechaDesde) {
-	this.fechaDesde = fechaDesde;
-    }
-
-    public Date getFechaHasta() {
-	return fechaHasta;
-    }
-
-    public void setFechaHasta(Date fechaHasta) {
-	this.fechaHasta = fechaHasta;
-    }
-
-    public DetalleEstadia getEstadiaSelected() {
-	return estadiaSelected;
-    }
-
-    public void setEstadiaSelected(DetalleEstadia estadiaSelected) {
-	TransaccionPlayaManagedBean.estadiaSelected = estadiaSelected;
-    }
-
-    public SelectItem[] getTipoPagoOptions() {
-	if (tipoPagoOptions == null) {
-	    List<TipoPago> tiposPago = new ArrayList<TipoPago>();
-	    tiposPago.addAll(getTipoPagoService().findAll());
-	    this.tipoPagoOptions = new SelectItem[tiposPago.size() + 1];
-	    tipoPagoOptions[0] = new SelectItem("", "Todos");
-
-	    for (int i = 0; i < tiposPago.size(); i++) {
-		tipoPagoOptions[i + 1] = new SelectItem(tiposPago.get(i), tiposPago.get(i).getNombre());
-	    }
+		return results;
 	}
 
-	return tipoPagoOptions;
-    }
+	public List<Playa> completePlayas(String query) {
 
-    public void setTipoPagoOptions(SelectItem[] tipoPagoOptions) {
-	this.tipoPagoOptions = tipoPagoOptions;
-    }
+		List<Playa> playas = this.getPlayaService().findByLikeNombreComercial(
+				query);
 
-    public void listadoDetalleTransaccionPDF(Object document) throws IOException, BadElementException,
-	    DocumentException {
+		return playas;
+	}
 
-	Font fuenteNegra18 = new Font(Font.TIMES_ROMAN, 16, Font.NORMAL, Color.BLACK);
+	public List<TransaccionPlaya> updateTransaccionesPorFechaList() {
+		Date fechaDesde = (this.fechaDesde != null ? this.fechaDesde
+				: new Date(01012012));
+		Date fechaHasta = (this.fechaHasta != null ? this.fechaHasta : Calendar
+				.getInstance().getTime());
 
-	Paragraph titulo = new Paragraph();
-	titulo.add(new Paragraph("Detalle de transacciones de la playa "
-		+ liquidacionSelected.getEstadia().getPlaya().getRazonSocial() + " emitido el " + fechaActual(),
-		fuenteNegra18));
-	agregarLineasEnBlanco(titulo, 2);
-	titulo.setAlignment(Element.ALIGN_CENTER);
-	String sep = File.separator;
-	Document pdf = (Document) document;
-	pdf.open();
-	pdf.setPageSize(PageSize.A4);
-	ExternalContext extContext = FacesContext.getCurrentInstance().getExternalContext();
-	String logo = extContext.getRealPath("resources" + sep + "images" + sep + "transacciones.png");
-	pdf.addTitle("Listado de empleados");
-	pdf.add(Image.getInstance(logo));
-	pdf.add(titulo);
-    }
+		Playa playa = getPlayaService().findByNombreComercial(this.acTxtPlaya);
+		CuentaPlaya cuentaPlaya = getCuentaPlayaService().findByPlaya(playa);
 
-    public void listadoGeneralTransaccionPDF(Object document) throws IOException, BadElementException,
-	    DocumentException {
+		transaccionesPorFechaList = getTransaccionPlayaService()
+				.findTransaccionesByFecha(cuentaPlaya, fechaDesde, fechaHasta);
+		return transaccionesPorFechaList;
+	}
 
-	Font fuenteNegra18 = new Font(Font.TIMES_ROMAN, 16, Font.BOLD, Color.BLACK);
+	public List<TransaccionPlaya> getTransaccionesPorFechaList() {
 
-	Paragraph titulo = new Paragraph();
-	titulo.add(new Paragraph("Listado de playas de estacionamiento liquidadas", fuenteNegra18));
-	agregarLineasEnBlanco(titulo, 2);
-	titulo.setAlignment(Element.ALIGN_CENTER);
-	String sep = File.separator;
-	Document pdf = (Document) document;
-	pdf.open();
-	pdf.setPageSize(PageSize.A4);
-	ExternalContext extContext = FacesContext.getCurrentInstance().getExternalContext();
-	String logo = extContext.getRealPath("resources" + sep + "images" + sep + "transacciones.png");
-	pdf.addTitle("Listado de empleados");
-	pdf.add(Image.getInstance(logo));
-	pdf.add(titulo);
-    }
+		return transaccionesPorFechaList;
+	}
 
-    public void listadoTransaccionesPDF(Object document) throws IOException, BadElementException, DocumentException {
+	public void setTransaccionesPorFechaList(
+			List<TransaccionPlaya> transaccionesPorFechaList) {
+		TransaccionPlayaManagedBean.transaccionesPorFechaList = transaccionesPorFechaList;
+	}
 
-	Font fuenteNegra18 = new Font(Font.TIMES_ROMAN, 16, Font.BOLD, Color.BLACK);
+	public void updateTransaccionesList() {
+		transaccionesList = getTransaccionPlayaService()
+				.findTransaccionesByLiquidacion(liquidacionSelected);
+	}
 
-	Paragraph titulo = new Paragraph();
-	titulo.add(new Paragraph("Listado de transacciones", fuenteNegra18));
-	agregarLineasEnBlanco(titulo, 2);
-	titulo.setAlignment(Element.ALIGN_CENTER);
-	String sep = File.separator;
-	Document pdf = (Document) document;
-	pdf.open();
-	pdf.setPageSize(PageSize.A4);
-	ExternalContext extContext = FacesContext.getCurrentInstance().getExternalContext();
-	String logo = extContext.getRealPath("resources" + sep + "images" + sep + "transacciones.png");
-	pdf.addTitle("Listado de transacciones");
-	pdf.add(Image.getInstance(logo));
-	pdf.add(titulo);
-    }
+	public List<TransaccionPlaya> getTransaccionesList() {
+		// transaccionesList =
+		// getTransaccionPlayaService().findTransaccionesByLiquidacion(liquidacionSelected);
+		if (transaccionesList == null) {
+			updateTransaccionesList();
+		}
+		return transaccionesList;
+	}
 
-    private static void agregarLineasEnBlanco(Paragraph parrafo, int nLineas) {
-	for (int i = 0; i < nLineas; i++)
-	    parrafo.add(new Paragraph(" "));
-    }
+	public void setTransaccionesList(List<TransaccionPlaya> transaccionesList) {
+		TransaccionPlayaManagedBean.transaccionesList = transaccionesList;
+	}
 
-    private String fechaActual() {
-	Date hoy = new Date();
-	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-	String fecha = formato.format(hoy);
-	return fecha;
+	public Liquidacion getLiquidacionSelected() {
+		return liquidacionSelected;
+	}
 
-    }
+	public void setLiquidacionSelected(Liquidacion liquidacionSelected) {
+		TransaccionPlayaManagedBean.liquidacionSelected = liquidacionSelected;
+	}
+
+	public ITransaccionPlayaService getTransaccionPlayaService() {
+		return transaccionPlayaService;
+	}
+
+	public void setTransaccionPlayaService(
+			ITransaccionPlayaService transaccionPlayaService) {
+		this.transaccionPlayaService = transaccionPlayaService;
+	}
+
+	public IPlayaService getPlayaService() {
+		return playaService;
+	}
+
+	public void setPlayaService(IPlayaService playaService) {
+		this.playaService = playaService;
+	}
+
+	public ICuentaPlayaService getCuentaPlayaService() {
+		return cuentaPlayaService;
+	}
+
+	public void setCuentaPlayaService(ICuentaPlayaService cuentaPlayaService) {
+		this.cuentaPlayaService = cuentaPlayaService;
+	}
+
+	public ITipoPagoService getTipoPagoService() {
+		return tipoPagoService;
+	}
+
+	public void setTipoPagoService(ITipoPagoService tipoPagoService) {
+		this.tipoPagoService = tipoPagoService;
+	}
+
+	public String getAcTxtPlaya() {
+		return acTxtPlaya;
+	}
+
+	public void setAcTxtPlaya(String acTxtPlaya) {
+		this.acTxtPlaya = acTxtPlaya;
+	}
+
+	public Date getFechaDesde() {
+		return fechaDesde;
+	}
+
+	public void setFechaDesde(Date fechaDesde) {
+		this.fechaDesde = fechaDesde;
+	}
+
+	public Date getFechaHasta() {
+		return fechaHasta;
+	}
+
+	public void setFechaHasta(Date fechaHasta) {
+		this.fechaHasta = fechaHasta;
+	}
+
+	public SelectItem[] getTipoPagoOptions() {
+		if (tipoPagoOptions == null) {
+			List<TipoPago> tiposPago = new ArrayList<TipoPago>();
+			tiposPago.addAll(getTipoPagoService().findAll());
+			this.tipoPagoOptions = new SelectItem[tiposPago.size() + 1];
+			tipoPagoOptions[0] = new SelectItem("", "Todos");
+
+			for (int i = 0; i < tiposPago.size(); i++) {
+				tipoPagoOptions[i + 1] = new SelectItem(tiposPago.get(i),
+						tiposPago.get(i).getNombre());
+			}
+		}
+
+		return tipoPagoOptions;
+	}
+
+	public void setTipoPagoOptions(SelectItem[] tipoPagoOptions) {
+		this.tipoPagoOptions = tipoPagoOptions;
+	}
+
+	public void listadoDetalleTransaccionPDF(Object document)
+			throws IOException, BadElementException, DocumentException {
+
+		Font fuenteNegra18 = new Font(Font.TIMES_ROMAN, 16, Font.NORMAL,
+				Color.BLACK);
+
+		Paragraph titulo = new Paragraph();
+		titulo.add(new Paragraph("Detalle de transacciones de la playa "
+				+ liquidacionSelected.getEstadia().getPlaya().getRazonSocial()
+				+ " emitido el " + fechaActual(), fuenteNegra18));
+		agregarLineasEnBlanco(titulo, 2);
+		titulo.setAlignment(Element.ALIGN_CENTER);
+		String sep = File.separator;
+		Document pdf = (Document) document;
+		pdf.open();
+		pdf.setPageSize(PageSize.A4);
+		ExternalContext extContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		String logo = extContext.getRealPath("resources" + sep + "images" + sep
+				+ "transacciones.png");
+		pdf.addTitle("Listado de empleados");
+		pdf.add(Image.getInstance(logo));
+		pdf.add(titulo);
+	}
+
+	public void listadoGeneralTransaccionPDF(Object document)
+			throws IOException, BadElementException, DocumentException {
+
+		Font fuenteNegra18 = new Font(Font.TIMES_ROMAN, 16, Font.BOLD,
+				Color.BLACK);
+
+		Paragraph titulo = new Paragraph();
+		titulo.add(new Paragraph(
+				"Listado de playas de estacionamiento liquidadas",
+				fuenteNegra18));
+		agregarLineasEnBlanco(titulo, 2);
+		titulo.setAlignment(Element.ALIGN_CENTER);
+		String sep = File.separator;
+		Document pdf = (Document) document;
+		pdf.open();
+		pdf.setPageSize(PageSize.A4);
+		ExternalContext extContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		String logo = extContext.getRealPath("resources" + sep + "images" + sep
+				+ "transacciones.png");
+		pdf.addTitle("Listado de empleados");
+		pdf.add(Image.getInstance(logo));
+		pdf.add(titulo);
+	}
+
+	public void listadoTransaccionesPDF(Object document) throws IOException,
+			BadElementException, DocumentException {
+
+		Font fuenteNegra18 = new Font(Font.TIMES_ROMAN, 16, Font.BOLD,
+				Color.BLACK);
+
+		Paragraph titulo = new Paragraph();
+		titulo.add(new Paragraph("Listado de transacciones", fuenteNegra18));
+		agregarLineasEnBlanco(titulo, 2);
+		titulo.setAlignment(Element.ALIGN_CENTER);
+		String sep = File.separator;
+		Document pdf = (Document) document;
+		pdf.open();
+		pdf.setPageSize(PageSize.A4);
+		ExternalContext extContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		String logo = extContext.getRealPath("resources" + sep + "images" + sep
+				+ "transacciones.png");
+		pdf.addTitle("Listado de transacciones");
+		pdf.add(Image.getInstance(logo));
+		pdf.add(titulo);
+	}
+
+	private static void agregarLineasEnBlanco(Paragraph parrafo, int nLineas) {
+		for (int i = 0; i < nLineas; i++)
+			parrafo.add(new Paragraph(" "));
+	}
+
+	private String fechaActual() {
+		Date hoy = new Date();
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		String fecha = formato.format(hoy);
+		return fecha;
+
+	}
+
+	public TransaccionPlaya getTransaccionSelected() {
+		return transaccionSelected;
+	}
+
+	public void setTransaccionSelected(TransaccionPlaya transaccionSelected) {
+		TransaccionPlayaManagedBean.transaccionSelected = transaccionSelected;
+	}
 
 }
