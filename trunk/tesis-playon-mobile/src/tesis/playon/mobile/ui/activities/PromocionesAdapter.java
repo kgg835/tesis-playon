09127d24d3097a073e7bb7635fdbb40dff616b54
morales.batovski <morales.batovski@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-06-01 19:44:26 +0000

    Android: + Adapter de lista de comentarios. + Adapter de lista de tarifas. + Adapter de lista de promociones.

---
+++ trunk/tesis-playon-mobile/src/tesis/playon/mobile/ui/activities/PromocionesAdapter.java
@@ -0,0 +1,61 @@
+package tesis.playon.mobile.ui.activities;
+
+import java.util.List;
+
+import tesis.playon.mobile.R;
+import tesis.playon.mobile.json.model.Promocion;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class PromocionesAdapter extends ArrayAdapter<Promocion> {
+
+    Context context;
+    int layoutResourceId;
+    List<Promocion> listaPromociones = null;
+
+    public PromocionesAdapter(Context context, int layoutResourceId, List<Promocion> listaPromociones) {
+	super(context, layoutResourceId, listaPromociones);
+	this.layoutResourceId = layoutResourceId;
+	this.context = context;
+	this.listaPromociones = listaPromociones;
+    }
+
+    static class ViewHolder {
+	TextView txtDescripcion;
+	TextView txtVehiculo;
+	TextView txtPrecio;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+	View row = convertView;
+	ViewHolder holder = null;
+
+	if (row == null) {
+	    LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+	    row = inflater.inflate(layoutResourceId, parent, false);
+
+	    holder = new ViewHolder();
+	    holder.txtDescripcion = (TextView) row.findViewById(R.id.txt_descripcion);
+	    holder.txtVehiculo = (TextView) row.findViewById(R.id.txt_vehiculo);
+	    holder.txtPrecio = (TextView) row.findViewById(R.id.txt_precio);
+
+	    row.setTag(holder);
+	} else {
+	    holder = (ViewHolder) row.getTag();
+	}
+
+	Promocion promocion = listaPromociones.get(position);
+	holder.txtDescripcion.setText(promocion.getDescripcion());
+	holder.txtVehiculo.setText(promocion.getTarifa().getCategoriaVehiculo().getNombre());
+	holder.txtPrecio.setText(promocion.getTarifa().getImporte().toString());
+
+	return row;
+    }
+
+}
