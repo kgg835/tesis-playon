72c2b048f21073012cd1ca30607e162719b41acc
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-06-09 16:27:20 +0000

    - Se introdujo el SP en el script principal. - Se modificó el tipo de datos de horaInicio y horaFin en la tabla promoción..

--- tesis-playon-db/db_script_structure+basicdata+user.sql
+++ tesis-playon-db/db_script_structure+basicdata+user.sql
@@ -131,8 +131,8 @@ CREATE TABLE `promocion` (
   `fechaAlta` datetime NOT NULL,
   `fechaFin` date DEFAULT NULL,
   `fechaInicio` date DEFAULT NULL,
-  `horaFin` date DEFAULT NULL,
-  `horaInicio` date DEFAULT NULL,
+  `horaFin` datetime DEFAULT NULL,
+  `horaInicio` datetime DEFAULT NULL,
   `montoFijo` float DEFAULT NULL,
   `nombre` varchar(50) NOT NULL,
   `tarifaID` int(11) NOT NULL,
@@ -961,6 +961,51 @@ CREATE TABLE `rol_usuario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 -- =================================================================== --
+
+
+-- =================================================================== --
+
+/**
+ * Creación del procedure
+ */
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `tesis_playon`.`busquedaAvanzada` $$
+CREATE PROCEDURE `tesis_playon`.`busquedaAvanzada` (
+IN platitud DOUBLE,
+IN plongitud DOUBLE,
+IN pTipoEstadia INT,
+IN pCategoriaVehiculo INT,
+IN pNombreComercial MEDIUMTEXT,
+IN pPromociones TINYINT
+)
+BEGIN
+    SELECT p.barrioID, p.cuit, p.disponibilidad, p.domicilio, p.telefono, p.email,
+        p.estadoPlayaID, p.nombreComercial, p.latitud, p.longitud, p.razonSocial,
+        p.playaID, p.url
+    FROM playa p
+    WHERE ((p.playaID IN (SELECT DISTINCT t.playaID
+                            FROM tarifa t
+                            WHERE (pTipoEstadia = 0 OR t.tipoEstadiaID = pTipoEstadia)
+                                AND (pCategoriaVehiculo = 0 OR t.categoriaVehiculoID = pCategoriaVehiculo)))
+            AND( pPromociones = 0 OR (p.playaID IN (SELECT DISTINCT promo.playaID
+                                                        FROM promocion promo
+                                                        WHERE pPromociones = 1
+                                                                AND promo.estadoPromocionID=2
+                                                                AND (now() >= promo.fechaInicio )
+                                                                AND (now() <= promo.fechaFin ))))
+            AND (p.estadoPlayaID = 2)
+            AND (p.disponibilidad > 0));
+END $$
+
+DELIMITER ;
+
+-- =================================================================== --
+
+
+-- =================================================================== --
+
 /**
  * Crear el usuario 'playonAdmin' con password por defecto 'playon'.
  */
