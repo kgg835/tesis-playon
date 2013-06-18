df16f03805572e9974a04a8162dfcd08dbe6a703
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-06-18 17:07:24 +0000

    auditoria publicidad

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/AuditoriaPublicidadesManagedBean.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/AuditoriaPublicidadesManagedBean.java
@@ -27,178 +27,228 @@ import tesis.playon.web.service.IPublicidadService;
 @ViewScoped
 public class AuditoriaPublicidadesManagedBean implements Serializable {
 
-    private static final long serialVersionUID = 1495139340595693159L;
+	private static final long serialVersionUID = 1495139340595693159L;
 
-    @ManagedProperty(value = "#{EstadoPublicidadService}")
-    IEstadoPublicidadService estadoPublicidadService;
+	@ManagedProperty(value = "#{EstadoPublicidadService}")
+	IEstadoPublicidadService estadoPublicidadService;
 
-    @ManagedProperty(value = "#{PublicidadService}")
-    IPublicidadService publicidadService;
+	@ManagedProperty(value = "#{PublicidadService}")
+	IPublicidadService publicidadService;
 
-    @ManagedProperty(value = "#{FotoPublicidadService}")
-    IFotoPublicidadService fotoPublicidadService;
+	@ManagedProperty(value = "#{FotoPublicidadService}")
+	IFotoPublicidadService fotoPublicidadService;
 
-    EstadoPublicidad estadoPendiente, estadoAprobado, estadoRechazado;
+	EstadoPublicidad estadoPendiente, estadoAprobado, estadoRechazado;
 
-    private static List<Publicidad> publicidadesPendientesList;
+	private static List<Publicidad> publicidadesPendientesList;
 
-    private static List<Publicidad> publicidadesAprobadasList;
+	private static List<Publicidad> publicidadesAprobadasList;
 
-    private static List<Publicidad> publicidadRechazadasList;
+	private static List<Publicidad> publicidadRechazadasList;
 
-    public static Publicidad publicidadSelected;
+	public static Publicidad publicidadSelected;
 
-    @PostConstruct
-    private void init() {
-	estadoPendiente = getEstadoPublicidadService().findByNombreEstadoPublicidad("Pendiente");
-	publicidadesPendientesList = getPublicidadService().findByEstado(estadoPendiente);
-	
-	estadoAprobado = getEstadoPublicidadService().findByNombreEstadoPublicidad("Aprobada");
-	publicidadesAprobadasList = getPublicidadService().findByEstado(estadoAprobado);
-	
-	estadoRechazado = getEstadoPublicidadService().findByNombreEstadoPublicidad("Rechazada");
-	publicidadRechazadasList = getPublicidadService().findByEstado(estadoRechazado);
-    }
+	@PostConstruct
+	private void init() {
+		estadoPendiente = getEstadoPublicidadService()
+				.findByNombreEstadoPublicidad("Pendiente");
+		publicidadesPendientesList = getPublicidadService().findByEstado(
+				estadoPendiente);
 
-    public void approvePublicidad() {
-	try {
-	    if (publicidadSelected != null) {
-		publicidadSelected.setEstado(estadoAprobado);
-		getPublicidadService().update(publicidadSelected);
+		estadoAprobado = getEstadoPublicidadService()
+				.findByNombreEstadoPublicidad("Aprobada");
+		publicidadesAprobadasList = getPublicidadService().findByEstado(
+				estadoAprobado);
 
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
-			"Se aprobó la publicidad de la empresa: " + publicidadSelected.getNombreEmpresa(), "");
-		FacesContext.getCurrentInstance().addMessage(null, message);
-	    }
-	} catch (Exception e) {
-	    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR
-		    , "Error, no se pudo aprobar la publicidad de la empresa: "
-		    + publicidadSelected.getNombreEmpresa(), "Por favor, inténtelo más tarde.");
-	    FacesContext.getCurrentInstance().addMessage(null, message);
+		estadoRechazado = getEstadoPublicidadService()
+				.findByNombreEstadoPublicidad("Rechazada");
+		publicidadRechazadasList = getPublicidadService().findByEstado(
+				estadoRechazado);
 	}
-    }
-    
-    public void rejectPublicidad(){
-	try {
-	    if (publicidadSelected != null) {
-		publicidadSelected.setEstado(estadoRechazado);
-		getPublicidadService().update(publicidadSelected);
 
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
-			"Se rechazó la publicidad de la empresa: " + publicidadSelected.getNombreEmpresa(), "");
-		FacesContext.getCurrentInstance().addMessage(null, message);
-	    }
-	} catch (Exception e) {
-	    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR
-		    , "Error, no se pudo rechazar la publicidad de la empresa: "
-		    + publicidadSelected.getNombreEmpresa(), "Por favor, inténtelo más tarde.");
-	    FacesContext.getCurrentInstance().addMessage(null, message);
+	public void approvePublicidad() {
+		try {
+			if (publicidadSelected != null) {
+				publicidadSelected.setEstado(estadoAprobado);
+				getPublicidadService().update(publicidadSelected);
+
+				FacesMessage message = new FacesMessage(
+						FacesMessage.SEVERITY_INFO,
+						"Se aprobó la publicidad de la empresa: "
+								+ publicidadSelected.getNombreEmpresa(), "");
+				FacesContext.getCurrentInstance().addMessage(null, message);
+			}
+
+			estadoPendiente = getEstadoPublicidadService()
+					.findByNombreEstadoPublicidad("Pendiente");
+			publicidadesPendientesList = getPublicidadService().findByEstado(
+					estadoPendiente);
+
+			estadoAprobado = getEstadoPublicidadService()
+					.findByNombreEstadoPublicidad("Aprobada");
+			publicidadesAprobadasList = getPublicidadService().findByEstado(
+					estadoAprobado);
+
+			estadoRechazado = getEstadoPublicidadService()
+					.findByNombreEstadoPublicidad("Rechazada");
+			publicidadRechazadasList = getPublicidadService().findByEstado(
+					estadoRechazado);
+
+		} catch (Exception e) {
+			FacesMessage message = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Error, no se pudo aprobar la publicidad de la empresa: "
+							+ publicidadSelected.getNombreEmpresa(),
+					"Por favor, inténtelo más tarde.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
 	}
-    }
 
-    /**
-     * @return the estadoPublicidadService
-     */
-    public IEstadoPublicidadService getEstadoPublicidadService() {
-	return estadoPublicidadService;
-    }
+	public void rejectPublicidad() {
+		try {
+			if (publicidadSelected != null) {
+				publicidadSelected.setEstado(estadoRechazado);
+				getPublicidadService().update(publicidadSelected);
 
-    /**
-     * @param estadoPublicidadService
-     *            the estadoPublicidadService to set
-     */
-    public void setEstadoPublicidadService(IEstadoPublicidadService estadoPublicidadService) {
-	this.estadoPublicidadService = estadoPublicidadService;
-    }
+				FacesMessage message = new FacesMessage(
+						FacesMessage.SEVERITY_INFO,
+						"Se rechazó la publicidad de la empresa: "
+								+ publicidadSelected.getNombreEmpresa(), "");
+				FacesContext.getCurrentInstance().addMessage(null, message);
+			}
 
-    /**
-     * @return the publicidadService
-     */
-    public IPublicidadService getPublicidadService() {
-	return publicidadService;
-    }
+			estadoPendiente = getEstadoPublicidadService()
+					.findByNombreEstadoPublicidad("Pendiente");
+			publicidadesPendientesList = getPublicidadService().findByEstado(
+					estadoPendiente);
 
-    /**
-     * @param publicidadService
-     *            the publicidadService to set
-     */
-    public void setPublicidadService(IPublicidadService publicidadService) {
-	this.publicidadService = publicidadService;
-    }
+			estadoAprobado = getEstadoPublicidadService()
+					.findByNombreEstadoPublicidad("Aprobada");
+			publicidadesAprobadasList = getPublicidadService().findByEstado(
+					estadoAprobado);
 
-    /**
-     * @return the fotoPublicidadService
-     */
-    public IFotoPublicidadService getFotoPublicidadService() {
-	return fotoPublicidadService;
-    }
+			estadoRechazado = getEstadoPublicidadService()
+					.findByNombreEstadoPublicidad("Rechazada");
+			publicidadRechazadasList = getPublicidadService().findByEstado(
+					estadoRechazado);
+		} catch (Exception e) {
+			FacesMessage message = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Error, no se pudo rechazar la publicidad de la empresa: "
+							+ publicidadSelected.getNombreEmpresa(),
+					"Por favor, inténtelo más tarde.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
 
-    /**
-     * @param fotoPublicidadService
-     *            the fotoPublicidadService to set
-     */
-    public void setFotoPublicidadService(IFotoPublicidadService fotoPublicidadService) {
-	this.fotoPublicidadService = fotoPublicidadService;
-    }
+	/**
+	 * @return the estadoPublicidadService
+	 */
+	public IEstadoPublicidadService getEstadoPublicidadService() {
+		return estadoPublicidadService;
+	}
 
-    /**
-     * @return the publicidadesPendientesList
-     */
-    public List<Publicidad> getPublicidadesPendientesList() {
-	return publicidadesPendientesList;
-    }
+	/**
+	 * @param estadoPublicidadService
+	 *            the estadoPublicidadService to set
+	 */
+	public void setEstadoPublicidadService(
+			IEstadoPublicidadService estadoPublicidadService) {
+		this.estadoPublicidadService = estadoPublicidadService;
+	}
 
-    /**
-     * @param publicidadesPendientesList
-     *            the publicidadesPendientesList to set
-     */
-    public void setPublicidadesPendientesList(List<Publicidad> publicidadesPendientesList) {
-	AuditoriaPublicidadesManagedBean.publicidadesPendientesList = publicidadesPendientesList;
-    }
+	/**
+	 * @return the publicidadService
+	 */
+	public IPublicidadService getPublicidadService() {
+		return publicidadService;
+	}
 
-    /**
-     * @return the publicidadesAprobadasList
-     */
-    public List<Publicidad> getPublicidadesAprobadasList() {
-	return publicidadesAprobadasList;
-    }
+	/**
+	 * @param publicidadService
+	 *            the publicidadService to set
+	 */
+	public void setPublicidadService(IPublicidadService publicidadService) {
+		this.publicidadService = publicidadService;
+	}
 
-    /**
-     * @param publicidadesAprobadasList
-     *            the publicidadesAprobadasList to set
-     */
-    public void setPublicidadesAprobadasList(List<Publicidad> publicidadesAprobadasList) {
-	AuditoriaPublicidadesManagedBean.publicidadesAprobadasList = publicidadesAprobadasList;
-    }
+	/**
+	 * @return the fotoPublicidadService
+	 */
+	public IFotoPublicidadService getFotoPublicidadService() {
+		return fotoPublicidadService;
+	}
 
-    /**
-     * @return the publicidadRechazadasList
-     */
-    public List<Publicidad> getPublicidadRechazadasList() {
-	return publicidadRechazadasList;
-    }
+	/**
+	 * @param fotoPublicidadService
+	 *            the fotoPublicidadService to set
+	 */
+	public void setFotoPublicidadService(
+			IFotoPublicidadService fotoPublicidadService) {
+		this.fotoPublicidadService = fotoPublicidadService;
+	}
 
-    /**
-     * @param publicidadRechazadasList
-     *            the publicidadRechazadasList to set
-     */
-    public void setPublicidadRechazadasList(List<Publicidad> publicidadRechazadasList) {
-	AuditoriaPublicidadesManagedBean.publicidadRechazadasList = publicidadRechazadasList;
-    }
+	/**
+	 * @return the publicidadesPendientesList
+	 */
+	public List<Publicidad> getPublicidadesPendientesList() {
+		return publicidadesPendientesList;
+	}
 
-    /**
-     * @return the publicidadSelected
-     */
-    public Publicidad getPublicidadSelected() {
-	return publicidadSelected;
-    }
+	/**
+	 * @param publicidadesPendientesList
+	 *            the publicidadesPendientesList to set
+	 */
+	public void setPublicidadesPendientesList(
+			List<Publicidad> publicidadesPendientesList) {
+		AuditoriaPublicidadesManagedBean.publicidadesPendientesList = publicidadesPendientesList;
+	}
 
-    /**
-     * @param publicidadSelected
-     *            the publicidadSelected to set
-     */
-    public void setPublicidadSelected(Publicidad publicidadSelected) {
-	AuditoriaPublicidadesManagedBean.publicidadSelected = publicidadSelected;
-    }
+	/**
+	 * @return the publicidadesAprobadasList
+	 */
+	public List<Publicidad> getPublicidadesAprobadasList() {
+		return publicidadesAprobadasList;
+	}
+
+	/**
+	 * @param publicidadesAprobadasList
+	 *            the publicidadesAprobadasList to set
+	 */
+	public void setPublicidadesAprobadasList(
+			List<Publicidad> publicidadesAprobadasList) {
+		AuditoriaPublicidadesManagedBean.publicidadesAprobadasList = publicidadesAprobadasList;
+	}
+
+	/**
+	 * @return the publicidadRechazadasList
+	 */
+	public List<Publicidad> getPublicidadRechazadasList() {
+		return publicidadRechazadasList;
+	}
+
+	/**
+	 * @param publicidadRechazadasList
+	 *            the publicidadRechazadasList to set
+	 */
+	public void setPublicidadRechazadasList(
+			List<Publicidad> publicidadRechazadasList) {
+		AuditoriaPublicidadesManagedBean.publicidadRechazadasList = publicidadRechazadasList;
+	}
+
+	/**
+	 * @return the publicidadSelected
+	 */
+	public Publicidad getPublicidadSelected() {
+		return publicidadSelected;
+	}
+
+	/**
+	 * @param publicidadSelected
+	 *            the publicidadSelected to set
+	 */
+	public void setPublicidadSelected(Publicidad publicidadSelected) {
+		AuditoriaPublicidadesManagedBean.publicidadSelected = publicidadSelected;
+	}
 
 }
