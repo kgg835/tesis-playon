3b1d47f7558d9eaed6844ee3ebcd43fc9bbe3092
gonzaloarribere@gmail.com <gonzaloarribere@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-06-23 14:13:12 +0000

    ALta de marca vehiculo, modificacion de menu administrador. --falta editar marca(no se va a eliminar) --falta alta de modelo de vehiculo

--- trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/ModeloVehiculoManagedBean.java
+++ trunk/tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/ModeloVehiculoManagedBean.java
@@ -7,14 +7,20 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.springframework.dao.DataAccessException;
 
 import tesis.playon.web.model.MarcaVehiculo;
 import tesis.playon.web.model.ModeloVehiculo;
 import tesis.playon.web.service.IMarcaVehiculoService;
 import tesis.playon.web.service.IModeloVehiculoService;
+import tesis.playon.web.service.IRolUsuarioService;
 
 /**
  * @author pablo
@@ -24,98 +30,178 @@ import tesis.playon.web.service.IModeloVehiculoService;
 @ViewScoped
 public class ModeloVehiculoManagedBean implements Serializable {
 
-    private static final long serialVersionUID = -1085389423375986168L;
+	private static final long serialVersionUID = -1085389423375986168L;
 
-    @ManagedProperty(value = "#{ModeloVehiculoService}")
-    IModeloVehiculoService modeloVehiculoService;
+	@ManagedProperty(value = "#{ModeloVehiculoService}")
+	IModeloVehiculoService modeloVehiculoService;
 
-    @ManagedProperty(value = "#{MarcaVehiculoService}")
-    IMarcaVehiculoService marcaVehiculoService;
+	@ManagedProperty(value = "#{MarcaVehiculoService}")
+	IMarcaVehiculoService marcaVehiculoService;
 
-    private ModeloVehiculo modelo;
+	private ModeloVehiculo modelo;
 
-    private MarcaVehiculo marca;
-    
-    @SuppressWarnings("unused")
-    private ModeloVehiculo modeloNulo;
-    
-    @SuppressWarnings("unused")
-    private MarcaVehiculo marcaNula;
-    
-    private List<MarcaVehiculo> marcasList;
-    
-    private List<ModeloVehiculo> modelosList;
+	private MarcaVehiculo marca;
 
-    public IModeloVehiculoService getModeloVehiculoService() {
-	return modeloVehiculoService;
-    }
+	@SuppressWarnings("unused")
+	private ModeloVehiculo modeloNulo;
 
-    public void setModeloVehiculoService(IModeloVehiculoService modeloVehiculoService) {
-	this.modeloVehiculoService = modeloVehiculoService;
-    }
+	@SuppressWarnings("unused")
+	private MarcaVehiculo marcaNula;
 
-    public IMarcaVehiculoService getMarcaVehiculoService() {
-	return marcaVehiculoService;
-    }
+	private List<MarcaVehiculo> marcasList;
 
-    public void setMarcaVehiculoService(IMarcaVehiculoService marcaVehiculoService) {
-	this.marcaVehiculoService = marcaVehiculoService;
-    }
+	private List<ModeloVehiculo> modelosList;
 
-    public ModeloVehiculo getModelo() {
-	return modelo;
-    }
+	private static final String LISTA_MARCA_VEHICULOS = "marcavehiculolist";
 
-    public void setModelo(ModeloVehiculo modelo) {
-	this.modelo = modelo;
-    }
+	private static final String ERROR = "error";
 
-    public MarcaVehiculo getMarca() {
-	return marca;
-    }
+	private static List<MarcaVehiculo> marcaVehiculoList;
 
-    public void setMarca(MarcaVehiculo marca) {
-	this.marca = marca;
-    }
+	private String nombreMarca;
 
-    public ModeloVehiculo getModeloNulo() {
-        return null;
-    }
+	private static MarcaVehiculo marcaVehiculoSelected;
 
-    public void setModeloNulo(ModeloVehiculo modeloNulo) {
-        this.modeloNulo = modeloNulo;
-    }
+	private String descripcionMarca;
 
-    public MarcaVehiculo getMarcaNula() {
-	modelosList = new ArrayList<ModeloVehiculo>();
-        return null;
-    }
+	@PostConstruct
+	private void init() {
+		marcaVehiculoList = getMarcaVehiculoService().findAll();
+	}
 
-    public void setMarcaNula(MarcaVehiculo marcaNula) {
-        this.marcaNula = marcaNula;
-    }
+	public String addMarcaVehiculo() {
+		MarcaVehiculo marcaVehiculo = new MarcaVehiculo();
+		try {
 
-    public List<MarcaVehiculo> getMarcasList() {
-	marcasList = getMarcaVehiculoService().findAll();
-        return marcasList;
-    }
+			marcaVehiculo.setNombre(getNombreMarca());
+			marcaVehiculo.setDescripcion(getDescripcionMarca());
+			getMarcaVehiculoService().save(marcaVehiculo);
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Se agregó correctamente la marca: "
+							+ marcaVehiculo.getNombre(), null);
+			FacesContext.getCurrentInstance().addMessage(null, message);
 
-    public void setMarcasList(List<MarcaVehiculo> marcasList) {
-        this.marcasList = marcasList;
-    }
+			return LISTA_MARCA_VEHICULOS;
+		} catch (DataAccessException e) {
+			FacesMessage message = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Error, no se pudo agregar el rol: "
+							+ marcaVehiculo.getNombre(),
+					"Por favor, inténtelo más tarde.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+			e.printStackTrace();
+		}
+		return ERROR;
+	}
 
-    public List<ModeloVehiculo> getModelosList() {
-        return modelosList;
-    }
+	public IModeloVehiculoService getModeloVehiculoService() {
+		return modeloVehiculoService;
+	}
 
-    public void setModelosList(List<ModeloVehiculo> modelosList) {
-        this.modelosList = modelosList;
-    }
+	public void setModeloVehiculoService(
+			IModeloVehiculoService modeloVehiculoService) {
+		this.modeloVehiculoService = modeloVehiculoService;
+	}
 
-    public void handleMarcaChange() {
-	if(marca != null && !marca.equals(""))
-	    modelosList = getModeloVehiculoService().findByMarca(marca);
-	else
-	    modelosList = new ArrayList<ModeloVehiculo>();
-    }
+	public IMarcaVehiculoService getMarcaVehiculoService() {
+		return marcaVehiculoService;
+	}
+
+	public void setMarcaVehiculoService(
+			IMarcaVehiculoService marcaVehiculoService) {
+		this.marcaVehiculoService = marcaVehiculoService;
+	}
+
+	public ModeloVehiculo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(ModeloVehiculo modelo) {
+		this.modelo = modelo;
+	}
+
+	public MarcaVehiculo getMarca() {
+		return marca;
+	}
+
+	public void setMarca(MarcaVehiculo marca) {
+		this.marca = marca;
+	}
+
+	public ModeloVehiculo getModeloNulo() {
+		return null;
+	}
+
+	public void setModeloNulo(ModeloVehiculo modeloNulo) {
+		this.modeloNulo = modeloNulo;
+	}
+
+	public MarcaVehiculo getMarcaNula() {
+		modelosList = new ArrayList<ModeloVehiculo>();
+		return null;
+	}
+
+	public void setMarcaNula(MarcaVehiculo marcaNula) {
+		this.marcaNula = marcaNula;
+	}
+
+	public List<MarcaVehiculo> getMarcasList() {
+		marcasList = getMarcaVehiculoService().findAll();
+		return marcasList;
+	}
+
+	public void setMarcasList(List<MarcaVehiculo> marcasList) {
+		this.marcasList = marcasList;
+	}
+
+	public List<ModeloVehiculo> getModelosList() {
+		return modelosList;
+	}
+
+	public void setModelosList(List<ModeloVehiculo> modelosList) {
+		this.modelosList = modelosList;
+	}
+
+	public void handleMarcaChange() {
+		if (marca != null && !marca.equals(""))
+			modelosList = getModeloVehiculoService().findByMarca(marca);
+		else
+			modelosList = new ArrayList<ModeloVehiculo>();
+	}
+
+	public List<MarcaVehiculo> getMarcaVehiculoList() {
+		return marcaVehiculoList;
+	}
+
+	public void setMarcaVehiculoList(List<MarcaVehiculo> marcaVehiculoList) {
+		ModeloVehiculoManagedBean.marcaVehiculoList = marcaVehiculoList;
+	}
+
+	public String getNombreMarca() {
+		return nombreMarca;
+	}
+
+	public void setNombreMarca(String nombreMarca) {
+		this.nombreMarca = nombreMarca;
+	}
+
+	public static MarcaVehiculo getMarcaVehiculoSelected() {
+		return marcaVehiculoSelected;
+	}
+
+	public static void setMarcaVehiculoSelected(
+			MarcaVehiculo marcaVehiculoSelected) {
+		ModeloVehiculoManagedBean.marcaVehiculoSelected = marcaVehiculoSelected;
+	}
+
+	public String getDescripcionMarca() {
+		return descripcionMarca;
+	}
+
+	public void setDescripcionMarca(String descripcionMarca) {
+		this.descripcionMarca = descripcionMarca;
+	}
+
+	/******************************** Metodos para marca de vehiculo ************************/
+
 }
