c6916265edd29ea3b95ceaae8eceaa78423ab4d8
morales.batovski <morales.batovski@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-07-08 13:23:07 +0000

    RESTful: + Incluida clase para la foto del perfil del cliente. + Corregida consulta para datos de la cuenta del cliente.

--- trunk/tesis-playon-restful/src/main/java/tesis/playon/restful/domain/Usuario.java
+++ trunk/tesis-playon-restful/src/main/java/tesis/playon/restful/domain/Usuario.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -18,132 +19,132 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Table(name = "usuario")
 public class Usuario implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "usuarioID")
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "usuarioID")
+    private Integer id;
 
-	@Column(name = "apellido")
-	private String apellido;
+    @Column(name = "apellido")
+    private String apellido;
 
-	@Column(name = "nombre")
-	private String nombre;
+    @Column(name = "nombre")
+    private String nombre;
 
-	@Column(name = "email")
-	private String email;
+    @Column(name = "email")
+    private String email;
 
-	@Column(name = "nroDoc", unique = true)
-	private int nroDoc;
+    @Column(name = "nroDoc", unique = true)
+    private int nroDoc;
 
-	@Column(name = "password")
-	private String password;
+    @Column(name = "password")
+    private String password;
 
-	@ManyToOne
-	@JoinColumn(name = "tipoDocID")
-	private TipoDoc tipoDoc;
+    @ManyToOne
+    @JoinColumn(name = "tipoDocID")
+    private TipoDoc tipoDoc;
 
-	@ManyToOne
-	@JoinColumn(name = "playaID")
-	private Playa playa;
+    @ManyToOne
+    @JoinColumn(name = "playaID")
+    private Playa playa;
 
-	@Column(name = "usuario", unique = true)
-	private String nombreUser;
+    @Column(name = "usuario", unique = true)
+    private String nombreUser;
 
-	@Column(name = "enable", columnDefinition = "TINYINT")
-	private Boolean enable;
+    @Column(name = "enable", columnDefinition = "TINYINT")
+    private Boolean enable;
 
-	// @Lob
-	// @Column(name = "fotoPerfil", columnDefinition = "mediumblob")
-	// private byte[] fotoPerfil;
+    @OneToOne
+    @JoinColumn(name = "fotoUsuarioID")
+    private FotoUsuario usuario;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+	return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+	this.id = id;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public String getApellido() {
+	return apellido;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public void setApellido(String apellido) {
+	this.apellido = apellido;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+	return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+	return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+	this.email = email;
+    }
 
-	public int getNroDoc() {
-		return nroDoc;
-	}
+    public int getNroDoc() {
+	return nroDoc;
+    }
 
-	public void setNroDoc(int nroDoc) {
-		this.nroDoc = nroDoc;
-	}
+    public void setNroDoc(int nroDoc) {
+	this.nroDoc = nroDoc;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+	return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+	this.password = password;
+    }
 
-	public TipoDoc getTipoDoc() {
-		return tipoDoc;
-	}
+    public TipoDoc getTipoDoc() {
+	return tipoDoc;
+    }
 
-	public void setTipoDoc(TipoDoc tipoDoc) {
-		this.tipoDoc = tipoDoc;
-	}
+    public void setTipoDoc(TipoDoc tipoDoc) {
+	this.tipoDoc = tipoDoc;
+    }
 
-	public Playa getPlaya() {
-		return playa;
-	}
+    public Playa getPlaya() {
+	return playa;
+    }
 
-	public void setPlaya(Playa playa) {
-		this.playa = playa;
-	}
+    public void setPlaya(Playa playa) {
+	this.playa = playa;
+    }
 
-	public String getNombreUser() {
-		return nombreUser;
-	}
+    public String getNombreUser() {
+	return nombreUser;
+    }
 
-	public void setNombreUser(String nombreUser) {
-		this.nombreUser = nombreUser;
-	}
+    public void setNombreUser(String nombreUser) {
+	this.nombreUser = nombreUser;
+    }
 
-	public Boolean getEnable() {
-		return enable;
-	}
+    public Boolean getEnable() {
+	return enable;
+    }
 
-	public void setEnable(Boolean enable) {
-		this.enable = enable;
-	}
+    public void setEnable(Boolean enable) {
+	this.enable = enable;
+    }
 
-	// public byte[] getFotoPerfil() {
-	// return fotoPerfil;
-	// }
-	//
-	// public void setFotoPerfil(byte[] fotoPerfil) {
-	// this.fotoPerfil = fotoPerfil;
-	// }
+    public FotoUsuario getUsuario() {
+	return usuario;
+    }
+
+    public void setUsuario(FotoUsuario usuario) {
+	this.usuario = usuario;
+    }
 
 }
