566fc8d188348b52781db2fd359df5fd50e02178
pablo.moreno.la31@gmail.com <pablo.moreno.la31@gmail.com@08926e26-378b-e419-6883-ee1adf1d9dd0>
2013-07-14 02:44:40 +0000

    - Listado de las comisiones de las transacciones! * falta terminar..

--- tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/EstadisticasAdministracionManagedBean.java
+++ tesis-playon-web/src/main/java/tesis/playon/web/managed/bean/EstadisticasAdministracionManagedBean.java
@@ -3,7 +3,9 @@
  */
 package tesis.playon.web.managed.bean;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,17 +16,31 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 
+import tesis.playon.web.model.Usuario;
 import tesis.playon.web.service.ILiquidacionService;
 import tesis.playon.web.service.IPublicidadService;
+import tesis.playon.web.service.IUsuarioService;
 
+import com.lowagie.text.BadElementException;
 import com.lowagie.text.Document;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
 import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.ColumnText;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
@@ -42,6 +58,9 @@ public class EstadisticasAdministracionManagedBean implements Serializable {
 
     @ManagedProperty(value = "#{PublicidadService}")
     IPublicidadService publicidadService;
+
+    @ManagedProperty(value = "#{UsuarioService}")
+    IUsuarioService usuarioService;
 
     private List<String[]> comisionesList, comisionesPublicidadList;
 
@@ -84,32 +103,132 @@ public class EstadisticasAdministracionManagedBean implements Serializable {
 
     public void crearPDF() {
 
-	Date hoy = new Date();
-	SimpleDateFormat formato = new SimpleDateFormat("yyyyMMdd");
-
 	Document documento = new Document(new Rectangle(PageSize.A4.rotate()));
 	FileOutputStream archivoPdf;
 
+	Date hoy = new Date();
+	SimpleDateFormat formato = new SimpleDateFormat("yyyyMMdd");
+
+	// Declaramos la imagen y texto de la cabecera
+	Phrase txtCabeceraFecha = null;
+	Phrase txtCabeceraUsuario = null;
+	Image imagen = null;
+	Font fuente = FontFactory.getFont("arial",12,Font.BOLDITALIC);
+
 	try {
 	    archivoPdf = new FileOutputStream("/tmp/informe_" + formato.format(hoy) + ".pdf");
-
-	    PdfWriter.getInstance(documento, archivoPdf).setInitialLeading(20);
-
+	    PdfWriter writer = PdfWriter.getInstance(documento, archivoPdf);
 	    documento.open();
-	    Image img = Image.getInstance(getClass().getResource("/images/logo_playon_admin.png"));
-	    img.scaleToFit(PageSize.A4.getHeight() * 1.3F, PageSize.A4.getWidth() * 1.3F);
-	    img.setAbsolutePosition(0.0F, 0.0F);
+	    documento.setMargins(72.0f, 72.0f, 72.0f, 72.0f);
 
-	    documento.add(img);
-	    
-	    
-	    
-	    
-	    
-	    
-	} catch (Exception ex) {
+	    /** setteamos la cabecera del documento **/
+	    try {
+		// Obtenemos la imagen
+		ExternalContext extContext = null;
+		extContext = FacesContext.getCurrentInstance().getExternalContext();
+		String sep = File.separator;
+		String path = extContext.getRealPath("resources" + sep + "images") 
+			+ sep + "logo_playon_admin.png";
+		imagen = Image.getInstance(path);
+		imagen.scaleToFit(280, 110);
+		
 
+		// Agregamos tambien un texto que acompañe la imagen
+		formato = new SimpleDateFormat("dd/MM/yyyy");
+		Usuario usuario = getUsuario();
+		String encabezadoFecha = formato.format(hoy);
+		txtCabeceraFecha = new Phrase(encabezadoFecha);
+		txtCabeceraFecha.setFont(fuente);
+
+		String encabezadoUsuario = usuario.getApellido() + ", " + usuario.getNombre();
+		txtCabeceraUsuario = new Phrase(encabezadoUsuario);
+		txtCabeceraUsuario.setFont(fuente);
+
+	    } catch (BadElementException e) {
+		e.printStackTrace();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+
+	    // Creamos un objeto PdfContentByte donde se guarda el contenido
+	    // de una página en el pdf. Gráficos y texto.
+	    PdfContentByte cb = writer.getDirectContent();
+
+
+	    // Esta es la imágen
+	    /*ColumnText.showTextAligned(cb, Element.ALIGN_LEFT, imgCabecera
+		    , (documento.right() - 200), documento.top(),0);*/
+
+	    // Este es el texto
+	    ColumnText.showTextAligned(cb, Element.ALIGN_RIGHT, txtCabeceraFecha,
+		    (documento.right() - 70), documento.top() + 10, 0);
+	    ColumnText.showTextAligned(cb, Element.ALIGN_RIGHT, txtCabeceraUsuario,
+		    (documento.right() - 70), documento.top() - 20, 0);
+
+	    documento.add(imagen);
+	    
+	    PdfPTable tabla = new PdfPTable(4);
+	    tabla.setWidthPercentage(100);
+	    
+	    PdfPCell celdaMes = new PdfPCell(new Paragraph("Mes", fuente));
+	    tabla.addCell(celdaMes);
+	    
+	    PdfPCell celdaAnio = new PdfPCell(new Paragraph("Año", fuente));
+	    tabla.addCell(celdaAnio);
+	    
+	    PdfPCell celdaTransacciones = new PdfPCell(new Paragraph("Cantidad de transacciones", fuente));
+	    tabla.addCell(celdaTransacciones);
+	    
+	    PdfPCell celdaComision = new PdfPCell(new Paragraph("Comisión por el servicio", fuente));
+	    tabla.addCell(celdaComision);
+	    
+	    
+	    for (int i = 0; i < comisionesList.size(); i++)
+	    {
+		String [] vComision = comisionesList.get(i);
+		tabla.addCell(convertirMes(vComision[1]));
+		tabla.addCell(vComision[0]);
+	    	tabla.addCell(vComision[2]);
+	    	tabla.addCell("$ "+ vComision[3]);
+	    	
+	    }
+	    
+	    documento.add(new Paragraph("\n"));
+	    documento.add(tabla);
+	    documento.close();
+	    writer.close();
+
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
+
+    private Usuario getUsuario() {
+	FacesContext facesContext = FacesContext.getCurrentInstance();
+	String userName = facesContext.getExternalContext().getRemoteUser();
+	Usuario user = getUsuarioService().findByNombreUsuario(userName);
+	return user;
+    }
+
+    private String convertirMes(String mesEnNumero) {
+	String[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto"
+		,"Septiembre", "Octubre", "Noviembre", "Diciembre" };
+	return meses[Integer.parseInt(mesEnNumero)-1];
+    }
+
+    /**
+     * @return the usuarioService
+     */
+    public IUsuarioService getUsuarioService() {
+	return usuarioService;
+    }
+
+    /**
+     * @param usuarioService
+     *            the usuarioService to set
+     */
+    public void setUsuarioService(IUsuarioService usuarioService) {
+	this.usuarioService = usuarioService;
     }
 
     /**
